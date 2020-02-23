5479978d18e20fc5abb9cb329afc813e5afd8e47
Spane Boswell <spaneboswell101@gmail.com>
2020-02-23 10:05:21 -0800

    Make mobile & desktop significantly more responsive on dbinfo.

--- dbinfo.php
+++ dbinfo.php
@@ -3,8 +3,8 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <?php include 'functions.php';?>
   <title>Findlater Database</title>
+  <?php include 'functions.php';?>
   <?php if(isMobile()){
 	  echo '<link rel="stylesheet" href="styles/m-dbinfo.css">';
   } else {
@@ -28,7 +28,21 @@ $longitude = file_get_contents('dustbin\longitude.txt');
 $sql = "SELECT DISTINCT *, (3959 * ACOS(COS(RADIANS($latitude)) * COS(RADIANS(latitude)) * COS(RADIANS(longitude) - RADIANS($longitude)) + SIN(RADIANS($latitude)) * SIN(RADIANS(latitude)))) AS DISTANCE FROM findlater ORDER BY distance";
 $result = mysqli_query($conn, $sql); // First parameter is just return of "mysqli_connect()" function
 
-echo "<table border='1'>";
+echo "<table class='TFtable' border='1'>";
+echo '<tr><td>eNB ID';
+echo '<td>Carrier';
+echo '<td>Type';
+echo '<td>Latitude';
+echo '<td>Longitude';
+echo '<td>First Seen';
+echo '<td>Band(s)';
+echo '<td>Citiy';
+echo '<td>Zip';
+echo '<td>State';
+echo '<td>Address';
+echo '<td>Link';
+echo '<td>Bio';
+echo '</tr>';
 while ($row = mysqli_fetch_assoc($result)) { // Important line !!! Check summary get row on array ..
     echo "<tr>";
     foreach ($row as $field => $value) { // I you want you can right this line like this: foreach($row as $value) {
