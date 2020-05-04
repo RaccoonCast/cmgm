8d692eabe59f4df5afb828a9e943cc66ef75df09
Spane Boswell <spaneboswell101@gmail.com>
2020-05-03 23:49:03 -0700

    cleanup cookiephp

--- cookie.php
+++ cookie.php
@@ -2,14 +2,17 @@
 <html lang="en">
    <head>
 	  <?php
+     if (isset($_GET['key'])) $key = $_GET['key'];
      include "functions.php"?>
    </head>
    <body>
-      <form action="cookie-2.php" method="get" autocomplete="off">
+      <form action="cookie.php" method="get" autocomplete="off">
          <p>Google Maps API Key: </p>
          <input type="text" name="key" id="txtresult" required>
          <input type="submit" class="submitbutton" style="color: #A8B2B1;"  value="Submit">
       </form>
-      <script src="js/index.js"></script>
+      <script>
+      document.cookie = "api_key=<?php echo $key;?>; expires=Thu, 18 Dec 2020 12:00:00 UTC; path=/";
+      </script>
    </body>
 </html>
