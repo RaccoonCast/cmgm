e2b495ae25fc4dfab99e371b56dd7f99d4d2831e
Spane Boswell <spaneboswell101@gmail.com>
2020-07-06 22:24:16 -0700

    Revert "Cleanup some junk on db-form"
    This reverts commit f656e9b8a3d61b4dff34f612777632ad7fd0b8c1.

--- database/database-form.php
+++ database/database-form.php
@@ -37,7 +37,7 @@
           </select>
           <br>
           <div style="padding: 8px ">
-          <input type="checkbox" id="multiple_carriers" name="carrier_multiple">
+          <input type="checkbox" id="multiple_carriers" name="carrier_multiple" value="true">
           <label for="multiple_carriers">Multiple carriers</label><br>
         </div>
      <p>eNB ID: </p>
@@ -49,21 +49,21 @@
         <?php
         }
         ?>
-        <input type="checkbox" id="ev1" name="permit_cellsite">
+        <input type="checkbox" id="ev1" name="permit_cellsite" value="true">
         <label for="ev1"> Permit says cellsite (+1)</label><br>
-        <input type="checkbox" id="ev2" name="permit_suspected_carrier">
+        <input type="checkbox" id="ev2" name="permit_suspected_carrier" value="true">
         <label for="ev2"> Permit matches suspected carrier (+20)</label><br>
-        <input type="checkbox" id="ev3" name="trails_match">
+        <input type="checkbox" id="ev3" name="trails_match" value="true">
         <label for="ev3"> Trails match suspected address with the suspected carrier (+5)</label><br>
-        <input type="checkbox" id="ev4" name="other_carriers_dont">
+        <input type="checkbox" id="ev4" name="other_carriers_dont" value="true">
         <label for="ev4"> Trails rule-out others (+3)</label><br>
-        <input type="checkbox" id="ev5" name="antennas_match_carrier">
+        <input type="checkbox" id="ev5" name="antennas_match_carrier" value="true">
         <label for="ev5"> Antennas look like suspected carrier (+1)</label><br>
-        <input type="checkbox" id="ev6" name="cellmapper_triangulation">
+        <input type="checkbox" id="ev6" name="cellmapper_triangulation" value="true">
         <label for="ev6"> CellMapper triangulates very close to the suspected location (+2)</label><br>
-        <input type="checkbox" id="ev7" name="image_evidence">
+        <input type="checkbox" id="ev7" name="image_evidence" value="true">
         <label for="ev7"> On-site image evidence of a site identifier matching the suspected carrier (+10)</label><br>
-        <input type="checkbox" id="ev8" name="verified_by_visit">
+        <input type="checkbox" id="ev8" name="verified_by_visit" value="true">
         <label for="ev8"> On-site verification (+5)</label><br>
     <p>Extra notes: </p>
 		<?php
