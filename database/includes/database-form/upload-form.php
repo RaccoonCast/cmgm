4c6a18db1b5d1dc4169f5c3091ef7dd1223d4953
Spane Boswell <spaneboswell101@gmail.com>
2020-07-07 04:24:26 -0700

    Sync image uploads & convert to jpgs & fix file size shit?

--- database/includes/database-form/upload-form.php
+++ database/includes/database-form/upload-form.php
@@ -14,7 +14,21 @@ if (isset($_POST['base64_file'])) {
   $data = base64_decode($img);
   $randomString = substr(str_shuffle(md5(time())),0,25);
   $file = UPLOAD_DIR . 'image-evidence-' . $randomString . '.png';
+  $fileNoExtension = UPLOAD_DIR . 'image-evidence-' . $randomString;
   $link = 'image-evidence-' . $randomString . '.png';
   $success = file_put_contents($file, $data);
+
+  $filePath = $file;
+  $image = imagecreatefrompng($filePath);
+  $bg = imagecreatetruecolor(imagesx($image), imagesy($image));
+  imagefill($bg, 0, 0, imagecolorallocate($bg, 255, 255, 255));
+  imagealphablending($bg, TRUE);
+  imagecopy($bg, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
+  imagedestroy($image);
+  $quality = 90; // 0 = worst / smaller file, 100 = better / bigger file
+  imagejpeg($bg, $fileNoExtension . ".jpg", $quality);
+  imagedestroy($bg);
+  unlink($file);
+  $link = 'image-evidence-' . $randomString . '.jpg';
 }
 ?>
