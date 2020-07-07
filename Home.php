99fdd606350b627eecc84f4805ccfd1355ac7eb7
Spane Boswell <spaneboswell101@gmail.com>
2020-07-06 18:02:19 -0700

    MOre

--- Home.php
+++ Home.php
@@ -7,17 +7,17 @@
    <body>
       <form action="convert.php" method="get" autocomplete="off">
          <p>Convert cm/gm/latlong</p>
-        <!-- <input class="fakeinput" type="text" name="data" value="<?php echo $latitude . ',' . $longitude . ',' . $carrier . ' '?>" id="txtresult" required> -->
         <?php
+        // Check if latitude & Longitude cookies exist - if they do don't require box to be filled - if they don't require box be filled
         if (isset($_COOKIE["latitude"]) | isset($_COOKIE["longitude"])) { ?>
           <input type="text" name="data" id="txtresult"> <?php
         } else { ?> <input type="text" name="data" id="txtresult" required> <?php
       } ?>
-
-
+        // Submit & Locate buttons
          <input type="submit" class="submitbutton" style="color: #000000;"  value="Submit">
 		     <input type="button" class="submitbutton" value="Locate" style="color: #00000;" onclick="myFunction()">
       </form>
+      // Latitude & longitude get script
       <script src="js/index.js"></script>
 	 <a id="findlater">FindlaterDB</a>
    </body>
