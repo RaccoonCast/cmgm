8d78a2cc35754d9c2d3fed9085fdad350cc2727e
Spane Boswell <spaneboswell101@gmail.com>
2020-07-08 02:30:29 -0700

    cleaner?

--- convert.php
+++ convert.php
@@ -4,9 +4,6 @@ $data = $_GET['data'];
 if (isset($_GET['latitude'])) $latitude = $_GET['latitude'];
 if (isset($_GET['longitude'])) $longitude = $_GET['longitude'];
 
-//for part of if statement for later
-$searchForValue = ',';
-
 // CellMapper URL Conversionhttps://www.cellmapper.net/map?MCC=310&MNC=260&type=LTE&latitude=40.13653501758051&longitude=-118.8975396188031&zoo.. > 40.136535,-118.897539)
 if (substr("$data", 0, 26) === 'https://www.cellmapper.net') {
     $input = substr("$data", 31, 97);
@@ -29,20 +26,17 @@ elseif(substr("$data", 0, 28) === 'https://www.google.com/maps/') {
   $latitude = $str_arr[0];
   $longitude = $str_arr[1];
   echo '<meta http-equiv="refresh" content="0; url=Hub.php?latitude=' . $latitude . '&longitude=' . $longitude . '">';
-  // Cookie latlong source
 }elseif (empty($_GET['data'])) {
-  echo '<meta http-equiv="refresh" content="0; url=Hub.php">';
-}
+// Cookie latlong source
+echo '<meta http-equiv="refresh" content="0; url=Hub.php">';
+} elseif( strpos($data, ',') !== false ) {
 // Comma Seperator (-50.45894508,-100.3848 > [-50.45894508] [-100.3848] )
-elseif( strpos($data, $searchForValue) !== false ) {
 $input_data = str_replace(' ', '', $data);
 $str_explode = explode(",", $input_data);
 $latitude = $str_explode[0];
 $longitude = $str_explode[1];
-$latitude = substr($latitude,0,10);
-$longitude = substr($longitude,0,10);
-echo '<meta http-equiv="refresh" content="0; url=Hub.php?latitude=' . $latitude . '&longitude=' . $longitude . '">';
-} else { // Google Maps search for the entered data (Burger King -> find closest burger king's LAT,LONG (from favorite location))
+} else {
+// Google Maps search for the entered data (Burger King -> find closest burger king's LAT,LONG (from favorite location))
  $data = str_replace(' ', '+', $data);
  $url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' . $data . '&location=' . $_COOKIE["latitude"] . ',' . $_COOKIE["longitude"] . '&radius=190000&key=' . $_COOKIE["api_key"] . '';
  $ch = curl_init();
@@ -55,10 +49,9 @@ echo '<meta http-equiv="refresh" content="0; url=Hub.php?latitude=' . $latitude 
  curl_close($ch);
 
  $response = json_decode($response);
-
  $latitude = $response->results[0]->geometry->location->lat;
  $longitude = $response->results[0]->geometry->location->lng;
- $latitude = substr($latitude,0,10);
- $longitude = substr($longitude,0,10);
- echo '<meta http-equiv="refresh" content="0; url=Hub.php?latitude=' . $latitude . '&longitude=' . $longitude . '">';
 }
+$latitude = substr($latitude,0,10);
+$longitude = substr($longitude,0,10);
+echo '<meta http-equiv="refresh" content="0; url=Hub.php?latitude=' . $latitude . '&longitude=' . $longitude . '">';
