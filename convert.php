8a930226ed4b89578ce0a2f92e4574f92821483d
Spane Boswell <spaneboswell101@gmail.com>
2020-07-08 03:03:33 -0700

    more shit on convert.php lol

--- convert.php
+++ convert.php
@@ -1,21 +1,20 @@
 <?php
-
 $data = $_GET['data'];
-if (isset($_GET['latitude'])) $latitude = $_GET['latitude'];
-if (isset($_GET['longitude'])) $longitude = $_GET['longitude'];
 
 // CellMapper URL Conversionhttps://www.cellmapper.net/map?MCC=310&MNC=260&type=LTE&latitude=40.13653501758051&longitude=-118.8975396188031&zoo.. > 40.136535,-118.897539)
 if (substr("$data", 0, 26) === 'https://www.cellmapper.net') {
     $input = substr("$data", 31, 97);
-    $str_explode = explode("&", $input);
-    $str1_explode = explode("=", $str_explode[0]);
-    $str2_explode = explode("=", $str_explode[1]);
-    $network = "$str1_explode[1]$str2_explode[1]";
+    $latitude = explode('latitude=', $data, 2)[1];
+    $longitude = explode('longitude=', $data, 2)[1];
+    $mcc = explode('MCC=', $data, 2)[1];
+    $mcc = substr($mcc,0,3);
+    $mnc = explode('MNC=', $data, 2)[1];
+    $mnc = substr($mnc,0,3);
+    $network = "$mcc$mnc";
     if('310260' == '' . $network . '') {$carrier = "T-Mobile";}
     if('310120' == '' . $network . '') {$carrier = "Sprint";}
     if('310410' == '' . $network . '') {$carrier = "ATT";}
     if('311480' == '' . $network . '') {$carrier = "Verizon";}
-    echo '<meta http-equiv="refresh" content="0; url=Hub.php?' . $str_explode[3] . '&' . $str_explode[4] . '&carrier=' . $carrier . '">';
 }
 // Google Maps URL Conversion (https://www.google.com/maps/@35.2820911,-111.0069811,15.21z > 35.2820911,-111.0069811 )
 elseif(substr("$data", 0, 28) === 'https://www.google.com/maps/') {
@@ -25,16 +24,10 @@ elseif(substr("$data", 0, 28) === 'https://www.google.com/maps/') {
   $str_arr = preg_split ("/\,/", $second);
   $latitude = $str_arr[0];
   $longitude = $str_arr[1];
-  echo '<meta http-equiv="refresh" content="0; url=Hub.php?latitude=' . $latitude . '&longitude=' . $longitude . '">';
-}elseif (empty($_GET['data'])) {
-// Cookie latlong source
-echo '<meta http-equiv="refresh" content="0; url=Hub.php">';
 } elseif( strpos($data, ',') !== false ) {
 // Comma Seperator (-50.45894508,-100.3848 > [-50.45894508] [-100.3848] )
-$input_data = str_replace(' ', '', $data);
-$str_explode = explode(",", $input_data);
-$latitude = $str_explode[0];
-$longitude = $str_explode[1];
+$latitude = explode(",", $data)[0];
+$longitude = explode(',', $data, 2)[1];
 } else {
 // Google Maps search for the entered data (Burger King -> find closest burger king's LAT,LONG (from favorite location))
  $data = str_replace(' ', '+', $data);
@@ -52,6 +45,9 @@ $longitude = $str_explode[1];
  $latitude = $response->results[0]->geometry->location->lat;
  $longitude = $response->results[0]->geometry->location->lng;
 }
+if(!isset($latitude)) $latitude = $_COOKIE["latitude"];
+if(!isset($longitude)) $longitude = $_COOKIE["longitude"];
+if(!isset($carrier)) $carrier = $_COOKIE["carrier"];
 $latitude = substr($latitude,0,10);
 $longitude = substr($longitude,0,10);
-echo '<meta http-equiv="refresh" content="0; url=Hub.php?latitude=' . $latitude . '&longitude=' . $longitude . '">';
+echo '<meta http-equiv="refresh" content="0; url=Hub.php?latitude=' . $latitude . '&longitude=' . $longitude . '&carrier=' . $carrier . '">';
