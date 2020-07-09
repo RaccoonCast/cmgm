e350a57d40b831821000c8d3dc4c9c9ad3895783
Spane Boswell <spaneboswell101@gmail.com>
2020-07-09 13:05:15 -0700

    solve double space issue & copy issue on mobile FOR REAL

--- Hub.php
+++ Hub.php
@@ -1,31 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
-      <?php include "functions.php"?>
+      <?php include "functions.php";
+            include "permits.php";
+             ?>
    </head>
    <body class="flex">
      <?php
-
+     if (!isset($_GET['permit_redirect'])) {
      echo ' Latitude: ';
      echo $latitude;
      echo '<br> Longitude: ';
      echo $longitude;
-       hubLatLong("HubFindlater.php","#F80000","Findlater","_self");
-       hubLatLong("HubDatabase.php","#00ccff","Database","_self");
-       hubLatLong("cm.php","#5DC904","CellMapper","_blank");
-       hubLatLong("gmaps.php","#4185FA","Google Maps","_blank");
-       ?>
-       <form target="_blank" action="Hub.php" method="get">
+     echo '<br> Address: ';
+     echo $address;
+      hubLatLong("HubFindlater.php","#F80000","Findlater","_self");
+      hubLatLong("HubDatabase.php","#00ccff","Database","_self");
+      hubLatLong("cm.php","#5DC904","CellMapper","_blank");
+      hubLatLong("gmaps.php","#4185FA","Google Maps","_blank");
+      ?>
+      <form target="_blank" action="Hub.php" method="get">
+        <input type="hidden" name="latitude" value="<?php echo $latitude;?>">
+        <input type="hidden" name="longitude" value="<?php echo $longitude;?>">
+        <input type="hidden" name="permit_redirect" value="true">
+        <input type="submit" onclick="copyToClipboard('<?php echo $address;?>')" class="submitbutton" style="color: #00e3e0;" value="Permits">
+      </form>
+      <form>
          <input type="hidden" name="latitude" value="<?php echo $latitude;?>">
          <input type="hidden" name="longitude" value="<?php echo $longitude;?>">
-         <input type="hidden" name="permit_redirect" value="true">
-         <input type="submit" onclick="copyToClipboard(copy)" class="submitbutton" style="color: #00e3e0;" value="Permits">
-       </form>
-       <?php
-       include "permits.php";
-       hubLatLong("\Home.php","#00000","Back","_self");
+         <input onclick="myFunction2()" style="color: #ffb700" type="submit" class="submitbutton" value="Copy">
+      </form>
+      <?php hubLatLong("\Home.php","#00000","Back","_self");
+    }?>
 
-?>
+
 <script src="js/copy.js"></script>
    </body>
 </html>
--- Permits.php
+++ Permits.php
@@ -1,5 +1,4 @@
 <?php
-if (isset($_GET['permit_redirect'])) {
 // Code I stole to get JSON data
 $url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='.trim($latitude).','.trim($longitude).'&key=' . $api_key . '';
 $ch = curl_init();
@@ -30,50 +29,38 @@ $response = json_decode($response);
     $directionCheck_b = explode(' ',trim($long_street_name));
 
     // Remove - North / East / South / West & N / E / S / W
-    if ("$directionCheck_a[0]" == "N" && "$directionCheck_b[0]" == "North") {$long_street_name = substr($long_street_name,5); }
-    if ("$directionCheck_a[0]" == "E" && "$directionCheck_b[0]" == "East")  {$long_street_name = substr($long_street_name,4); }
-    if ("$directionCheck_a[0]" == "S" && "$directionCheck_b[0]" == "South") {$long_street_name = substr($long_street_name,5); }
-    if ("$directionCheck_a[0]" == "W" && "$directionCheck_b[0]" == "West")  {$long_street_name = substr($long_street_name,4); }
+    if ("$directionCheck_a[0]" == "N" && "$directionCheck_b[0]" == "North") {$long_street_name = substr($long_street_name,6); }
+    if ("$directionCheck_a[0]" == "E" && "$directionCheck_b[0]" == "East")  {$long_street_name = substr($long_street_name,5); }
+    if ("$directionCheck_a[0]" == "S" && "$directionCheck_b[0]" == "South") {$long_street_name = substr($long_street_name,6); }
+    if ("$directionCheck_a[0]" == "W" && "$directionCheck_b[0]" == "West")  {$long_street_name = substr($long_street_name,5); }
 
     $street_name = $long_street_name;
 
     // Set short address variable
-    $text = "$number $street_name";
+    $address = "$number $street_name";
   ?>
 
-   <script>
-   // Copy short address to clipboard
-   var copy = "<?php echo $text ?>"
-   copyToClipboard(copy);
-
-    function copyToClipboard(text) {
-       var dummy = document.createElement("textarea");
-       document.body.appendChild(dummy);
-       dummy.value = text;
-       dummy.select();
-       document.execCommand("copy");
-       document.body.removeChild(dummy);
-   }
-  </script>
+   <script src="js/permit-copy.js"></script>
   <?php
   // Check what city we in and redirect to their page
-    if ("$city" == "Glendale") {
-      echo '<meta http-equiv="Refresh" content="0; url=https://csi.glendaleca.gov/csipropertyportal/" />';
-    } elseif ("$city" == "Los Angeles") {
-      echo '<meta http-equiv="Refresh" content="0; url=http://ladbsdoc.lacity.org/idispublic/" />';
-    } elseif ("$city" == "Long Beach") {
-      echo '<meta http-equiv="Refresh" content="0; url=http://citydocs.longbeach.gov/WebLink8/CustomSearch.aspx?SearchName=SearchbyAddress" />';
-    } elseif ("$city" == "Burbank") {
-      echo '<meta http-equiv="Refresh" content="0; url=https://permit.burbankca.gov/epalspi/" />';
-    } elseif ("$city" == "San Francisco") {
-      echo '<meta http-equiv="Refresh" content="0; url=https://dbiweb.sfgov.org/dbipts/default.aspx?page=AddressQuery" />';
-    } elseif ("$city" == "Pasadena") {
-      echo '<meta http-equiv="Refresh" content="0; url=https://eservices.cityofpasadena.net/iwrplandev/PropertySearch.aspx" />';
-    } else {
-      echo "QUERY: $url";
-      echo "$city not listed";
-    }
-    }
+  if (isset($_GET['permit_redirect'])) {
+  if ("$city" == "Glendale") {
+    echo '<meta http-equiv="Refresh" content="0; url=https://csi.glendaleca.gov/csipropertyportal/" />';
+  } elseif ("$city" == "Los Angeles") {
+    echo '<meta http-equiv="Refresh" content="0; url=http://ladbsdoc.lacity.org/idispublic/" />';
+  } elseif ("$city" == "Long Beach") {
+    echo '<meta http-equiv="Refresh" content="0; url=http://citydocs.longbeach.gov/WebLink8/CustomSearch.aspx?SearchName=SearchbyAddress" />';
+  } elseif ("$city" == "Burbank") {
+    echo '<meta http-equiv="Refresh" content="0; url=https://permit.burbankca.gov/epalspi/" />';
+  } elseif ("$city" == "San Francisco") {
+    echo '<meta http-equiv="Refresh" content="0; url=https://dbiweb.sfgov.org/dbipts/default.aspx?page=AddressQuery" />';
+  } elseif ("$city" == "Pasadena") {
+    echo '<meta http-equiv="Refresh" content="0; url=https://eservices.cityofpasadena.net/iwrplandev/PropertySearch.aspx" />';
+  } else {
+    echo "QUERY: $url";
+    echo "$city not listed";
+  }
+}
 ?>
 </body>
 </html>
