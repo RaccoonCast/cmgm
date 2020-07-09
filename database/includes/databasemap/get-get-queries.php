610a713b2ce1b32e67d8414d3081aa6b8538192e
Spane Boswell <spaneboswell101@gmail.com>
2020-07-09 10:38:38 -0700

    Remove type from database (I shudve done this forever ago)

--- database/includes/databasemap/get-get-queries.php
+++ database/includes/databasemap/get-get-queries.php
@@ -1,7 +1,6 @@
 <?php
-$sub1 = ""; $sub2 = ""; $sub3 = "";
+$sub1 = ""; $sub2 = "";
 if (isset($_GET['carrier'])) $carrier = $_GET['carrier'];
-if (isset($_GET['type'])) $type = $_GET['type'];
 if (isset($_GET['id'])) $id = $_GET['id'];
 
 if (!empty($carrier)) {
