38e07c9be303297c8b11342e0f69d7ed6af099d4
Spane Boswell <spaneboswell101@gmail.com>
2020-07-09 16:29:47 -0700

    reduce radius bc im trying to convince it to prefer addresses shorter to x

--- includes/convert/google-maps-conversion.php
+++ includes/convert/google-maps-conversion.php
@@ -1,7 +1,7 @@
 <?php
 // Google Maps search for the entered data (Burger King -> find closest burger king's LAT,LONG (from favorite location))
 $data = str_replace(' ', '+', $data);
-$url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' . $data . '&location=' . $_COOKIE["latitude"] . ',' . $_COOKIE["longitude"] . '&radius=10000&key=' . $_COOKIE["api_key"] . '';
+$url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' . $data . '&location=' . $_COOKIE["latitude"] . ',' . $_COOKIE["longitude"] . '&radius=1000&key=' . $_COOKIE["api_key"] . '';
 $ch = curl_init(); curl_setopt($ch, CURLOPT_URL, $url); curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); curl_setopt($ch, CURLOPT_PROXYPORT, 3128); curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 $response = curl_exec($ch);
 curl_close($ch);
