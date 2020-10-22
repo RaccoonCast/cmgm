0f0e7c990763a77b806b6b9af4ddb06a7cc1c4cb
Zack <XxZ4CK50xX@gmail.com>
2020-10-21 17:16:23 -0700

    aaaaaaaa
    aaaaaaaaaaa

--- Home.php
+++ Home.php
@@ -6,7 +6,7 @@
    </head>
    <body>
       <form action="convert.php" method="get" autocomplete="off">
-         <p class="same-line">Enter Location:<p class="small-text same-line" onclick="alert('What can you enter?\n\nCellMapper.NET URLs\n\Latitude,Longitude \(38.896,-77.036)\nGoogle Maps URLS\n\Building Address\nBuilding Name\nDMS Coordinates')">?</p></p>
+         <p class="same-line">Enter Location<p class="small-text same-line" onclick="alert('What can you enter?\n\nCellMapper.NET URLs\n\Latitude,Longitude \(38.896,-77.036)\nGoogle Maps URLS\n\Building Address\nBuilding Name\nDMS Coordinates')">?</p></p>
         <?php
         // Check if latitude & Longitude cookies exist - if they do don't require box to be filled - if they don't require box be filled
         if (isset($_COOKIE["latitude"]) | isset($_COOKIE["longitude"])) { ?>
@@ -14,10 +14,10 @@
         } else { ?> <input type="text" name="data" id="txtresult" required> <?php
       } ?>
         <!--- Submit & Locate buttons -->
-        <span class="buttons">
+        <div class="buttons">
             <input type="submit" class="submitbutton" style="color: #000000;"  value="Submit">
 		      <input type="button" class="submitbutton" value="Locate" style="color: #00000;" onclick="myFunction()">
-       </span>
+   </div>
       </form>
       <!-- Latitude & longitude get script -->
       <script src="js/index.js"></script>
