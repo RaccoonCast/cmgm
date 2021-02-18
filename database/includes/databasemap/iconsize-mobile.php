89aa92c66acba0766253505294e0cc74cc36a5f9
Spane Boswell <spaneboswell101@gmail.com>
2021-02-17 21:29:19 -0800

    yes i know its not yellow tyvm reader

--- database/includes/databasemap/iconsize-mobile.php
+++ database/includes/databasemap/iconsize-mobile.php
@@ -1,11 +1,17 @@
-var unverifiedVerified = L.icon({
+var unverified = L.icon({
     iconUrl: '../images/red.png',
     iconSize: [16, 16], // size of the icon
     popupAnchor: [0, -15]
 });
 
-var verifiedVerified = L.icon({
+var verified = L.icon({
     iconUrl: '../images/green.png',
+    iconSize: [16, 16], // size of the icon
+    popupAnchor: [0, -15]
+});
+
+var special = L.icon({
+    iconUrl: '../images/yellow.png',
     iconSize: [16, 16], // size of the icon
     popupAnchor: [0, -15]
 });
