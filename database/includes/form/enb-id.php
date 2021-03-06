10801efcdef9eae6f71baf293316fd6c919b2458
Spane Boswell <spaneboswell101@gmail.com>
2021-03-06 00:51:15 -0800

    Add other user located primary ID to form; tweak widths on multiple ID's

--- database/includes/form/enb-id.php
+++ database/includes/form/enb-id.php
@@ -4,6 +4,8 @@
 <textarea class="custominput ids inlineBlock" id="LTE_1" rows="1" cols="30" maxlength="7" placeholder="LTE_1" name="LTE_1"></textarea><span style="display: none;" id="multipleIds"><textarea class="custominput ids inlineBlock" id="LTE_2" rows="1" cols="30" maxlength="7" placeholder="LTE_2" name="LTE_2"></textarea><textarea class="custominput ids inlineBlock" id="LTE_3" rows="1" cols="30" maxlength="7" placeholder="LTE_3" name="LTE_3"></textarea><textarea class="custominput ids inlineBlock" id="LTE_4" rows="1" cols="30" maxlength="7" placeholder="LTE_4" name="LTE_4"></textarea><textarea class="custominput ids inlineBlock" id="LTE_5" rows="1" cols="30" maxlength="7" placeholder="LTE_5" name="LTE_5"></textarea><textarea class="custominput ids inlineBlock" id="LTE_6" rows="1" cols="30" maxlength="7" placeholder="LTE_6" name="LTE_6"></textarea><textarea class="custominput ids inlineBlock" id="NR_1" rows="1" cols="30" maxlength="7" placeholder="NR_1" name="NR_1"></textarea><textarea class="custominput ids inlineBlock" id="NR_2" rows="1" cols="30" maxlength="7" placeholder="NR_2" name="NR_2"></textarea><br>
 <input type="checkbox" id="pci_match" name="pci_match" value="true">
 <label for="pci_match">PCIs match on all IDs</label><br>
+<input type="checkbox" id="other_user_map_primary" name="other_user_map_primary" value="true">
+<label for="other_user_map_primary">Other user located primary ID</label><br>
 <input type="checkbox" id="id_pattern_match" name="id_pattern_match" value="verified">
 <label for="id_pattern_match">ID pattern on all IDs</label><br>
 <input style="margin-bottom: 12px;" type="checkbox" id="sector_match" name="sector_match" value="true">
@@ -34,12 +36,12 @@
   document.getElementById("multipleIds").removeAttribute('style');
 
   document.getElementById("extraENBID").setAttribute('style', 'display: none');
-  document.getElementById("LTE_1").setAttribute('style', 'width: 5.7%!important;');
-  document.getElementById("LTE_2").setAttribute('style', 'width: 5.7%!important;');
-  document.getElementById("LTE_3").setAttribute('style', 'width: 5.7%!important;');
-  document.getElementById("LTE_4").setAttribute('style', 'width: 5.7%!important;');
-  document.getElementById("LTE_5").setAttribute('style', 'width: 5.7%!important;');
-  document.getElementById("LTE_6").setAttribute('style', 'width: 5.7%!important;');
+  document.getElementById("LTE_1").setAttribute('style', 'width: 5.67%!important;');
+  document.getElementById("LTE_2").setAttribute('style', 'width: 5.67%!important;');
+  document.getElementById("LTE_3").setAttribute('style', 'width: 5.67%!important;');
+  document.getElementById("LTE_4").setAttribute('style', 'width: 5.67%!important;');
+  document.getElementById("LTE_5").setAttribute('style', 'width: 5.67%!important;');
+  document.getElementById("LTE_6").setAttribute('style', 'width: 5.67%!important;');
   document.getElementById("NR_1").setAttribute('style', 'width: 6%!important;');
   document.getElementById("NR_2").setAttribute('style', 'width: 6%!important;');
   }
