f4b142df704fd9e376f4107e5686d09b23795b84
Spane Boswell <spaneboswell101@gmail.com>
2021-03-08 08:29:02 -0800

    Fix footer links; Fix Map if carrier(and/or other vars) don't have values

--- database/includes/footer.php
+++ database/includes/footer.php
@@ -2,13 +2,15 @@
    $latitude = substr("$latitude", 0, 9);
    $longitude = substr("$longitude", 0, 10);
    $pmlink = "../Home.php?latitude=$latitude&longitude=$longitude&address=$address&zip=$zip&city=$city&state=$state&permit_redirect=true";
-   $dblink = "map.php?latitude=$latitude&longitude=$longitude&zoom=19";
-   $gm2link = "https://www.google.com/maps/d/u/0/viewer?hl=en&mid=1_69xTUt-g1MITj7lMs1oaCwKl1YuQ4nh&ll=$latitude%2C$longitude&z=19";
-   $gmlink = "../gmaps.php?latitude=$latitude&longitude=$longitude";
+   $cmlink = "../goto.php?goto_page=CellMapper&latitude=$latitude&longitude=$longitude";
+   $dblink = "../goto.php?goto_page=Database&latitude=$latitude&longitude=$longitude";
+   $gm2link = "../goto.php?goto_page=LA Permit Map&latitude=$latitude&longitude=$longitude";
+   $gmlink = "../goto.php?goto_page=Google Maps&latitude=$latitude&longitude=$longitude";
    ?>
 
    <footer>
      <a class="footerlink" onclick="copyToClipboard('<?php echo $address;?>')" href="<?php echo $pmlink?>">Permits</a>
+     <a class="footerlink" target="_blank" href="<?php echo $cmlink?>">CellMapper</a>
      <a class="footerlink" target="_blank" href="<?php echo $dblink?>">Maps</a>
      <a class="footerlink" target="_blank" href="<?php echo $gmlink?>">GMaps</a>
      <a class="footerlink" target="_blank" href="<?php echo $gm2link?>">Permits Map</a>
--- database/includes/map/get-get-queries.php
+++ database/includes/map/get-get-queries.php
@@ -5,7 +5,7 @@ $url_suffix = null;
 foreach($_GET as $key => $value){
   if ($key == "latitude" OR $key == "longitude" OR $key == "address" OR $key == "zip" OR $key == "city" OR $key == "state" OR $key == "zoom" OR $key == "limit") {
     ${$key} = $value;
-  } else {
+  } elseif(!empty($value)) {
     $url_suffix = $url_suffix . "&" . $key . "=" . $value;
 
     $trimExclamationMark = substr($value, 1);
