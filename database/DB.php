ddb6746edaf6da0340610c57f48fd8e054e41380
Spane Boswell <spaneboswell101@gmail.com>
2021-03-22 05:26:36 -0700

    iasodjasiodj recoding in db.php and use new EV calc; misc fix

--- database/DB.php
+++ database/DB.php
@@ -13,7 +13,7 @@ $limit = "500";
 $db_variables = "id > 0";
 
 foreach($_GET as $key => $value){
-  if ($key == "latitude" OR $key == "longitude") {
+  if ($key == "latitude" OR $key == "longitude" OR $key == "limit") {
     ${$key} = $value;
   } elseif ($key == "id") {
     ${$key} = $value;
@@ -23,12 +23,8 @@ foreach($_GET as $key => $value){
     $db_variables = $key . ' = "'.$value.'" AND ' . $db_variables;
   }
 }
-$database_get_list = "id,LTE_1,carrier,latitude,longitude,city,zip,state,address,bio,evidence_score,evidence_link";
+$database_get_list = "id,LTE_1,carrier,latitude,longitude,city,zip,state,address,bio,evidence_score,evidence_link,other_user_map_primary,permit_score,trails_match,carriers_dont_trail_match,antennas_match_carrier,cellmapper_triangulation,image_evidence,verified_by_visit,sector_split_match,archival_antenna_addition,only_reasonable_location,alt_carriers_here";
 
-$sql = "SELECT DISTINCT $database_get_list,
-(3959 * ACOS(COS(RADIANS($latitude)) * COS(RADIANS(latitude)) * COS(RADIANS(longitude) - RADIANS($longitude)) + SIN(RADIANS($latitude)) * SIN(RADIANS(latitude)))) AS DISTANCE
-FROM database_db WHERE $db_variables ORDER BY distance LIMIT $limit";
-$result = mysqli_query($conn, $sql); // First parameter is just return of "mysqli_connect()" function
 
 ?>
 <table border="1">
@@ -45,87 +41,76 @@ $result = mysqli_query($conn, $sql); // First parameter is just return of "mysql
 </thead>
 <tbody>
 <?php
-while ($row = mysqli_fetch_assoc($result)) { // Important line !!! Check summary get row on array ..
-  $colCount = 1;
-    echo "<tr>";
-    foreach ($row as $field => $value) {
-
-      $sepCount = ($colCount++);
-
-                  switch ($sepCount) {
-                    case 1:  $id = $value; break;
-                    case 2:  $LTE_1 = $value; break;
-                    case 3:  $carrier = $value; break;
-                    case 4:  $latitude = $value; break;
-                    case 5:  $longitude = $value; break;
-                    case 6:  $city = $value; break;
-                    case 7:  $zip = $value; break;
-                    case 8:  $state = $value; break;
-                    case 9:  $address = $value; break;
-                    case 10:  $bio = $value; break;
-                    case 11:  $evidence_score = $value; break;
-                    case 12:  $evidence_link = $value;
-                    $db_map_link = "https://cmgm.gq/database/Map.php?latitude=" . $latitude . "&longitude=" . $longitude . "&zoom=18";
-                    $cmlink = "../goto.php?goto_page=CellMapper&latitude=$latitude&longitude=$longitude";
-                    $gmlink = "../goto.php?goto_page=Google Maps&latitude=$latitude&longitude=$longitude";
-                    if($isMobile == "true") {
-                    echo "<td class=" . "lte" . " id=" . $id . ">" . $carrier . "<br><a href=" . "$cmlink" .">" . $LTE_1 . "</a></td>";
-                    } else {
-                    echo "<td class=" . "lte" . " id=" . $id . "><a href=" . "$cmlink" .">" . $LTE_1 . "</a></td>";
-                    echo "<td class=" . "carrier" . ">" . $carrier . "</td>";
-                    }
-                    if($isMobile == "true"){
-                      echo '<td class="address"><a href="' . $gmlink . '">' . $address . '</a></td>';
-                    } else {
-                      echo '<td class="address"><a href="' . $gmlink . '">' . $address . ' <br>' . $city . ', ' . $state . ' ' . $zip . '</a></td>';
-                    }
+$sql = "SELECT DISTINCT $database_get_list,
+(3959 * ACOS(COS(RADIANS($latitude)) * COS(RADIANS(latitude)) * COS(RADIANS(longitude) - RADIANS($longitude)) + SIN(RADIANS($latitude)) * SIN(RADIANS(latitude)))) AS DISTANCE
+FROM database_db WHERE $db_variables ORDER BY distance LIMIT $limit";
+$result = mysqli_query($conn,$sql);
+while($row = $result->fetch_assoc()) {
+    foreach ($row as $key => $value) {
+      $$key = $value;
+      if ($key == "alt_carriers_here") {
+        echo "<tr>";
+        $db_map_link = "https://cmgm.gq/database/Map.php?latitude=" . $latitude . "&longitude=" . $longitude . "&zoom=18";
+        $cmlink = "../goto.php?goto_page=CellMapper&latitude=$latitude&longitude=$longitude";
+        $gmlink = "../goto.php?goto_page=Google Maps&latitude=$latitude&longitude=$longitude";
+        if($isMobile == "true") {
+        echo "<td class=" . "lte" . " id=" . $id . ">" . $carrier . "<br><a href=" . "$cmlink" .">" . $LTE_1 . "</a></td>";
+        } else {
+        echo "<td class=" . "lte" . " id=" . $id . "><a href=" . "$cmlink" .">" . $LTE_1 . "</a></td>";
+        echo "<td class=" . "carrier" . ">" . $carrier . "</td>";
+        }
+        if($isMobile == "true"){
+          echo '<td class="address"><a href="' . $gmlink . '">' . $address . '</a></td>';
+        } else {
+          echo '<td class="address"><a href="' . $gmlink . '">' . $address . ' <br>' . $city . ', ' . $state . ' ' . $zip . '</a></td>';
+        }
 
 
-                    // start widget echo
-                    if($isMobile == "true"){
-                      echo nl2br("<td class=" . "bio" . ">" . $bio);
-                      if (!empty($bio)) echo '<br>';
-                      echo '<div class="widget-td">';
-                      echo '<a class="widget" href="SupplID.php?id='.$id.'"><abbr title="Add extra IDs">➕</abbr></a>';
-                      echo '<a class="widget" href="' . $db_map_link . '"><abbr title="View on Database Map">🌎</abbr></a>';
-                      echo '<a class="widget" href="Edit.php?id='.$id.'"><abbr title="Edit">🔧</abbr></a>';
-                      echo '<a class="widget" href="Delete.php?id='.$id.'"><abbr title="Delete">✂️</abbr></a>';
-                      echo '<a class="widget" href="Reader.php?back_url=DB&id='.$id.'"><abbr title="View all info">🔍</abbr></a></td>';
-                      } else {
-                      echo '<td class="widget-td" style="text-align: center;">';
-                      echo '<a class="widget" href="SupplID.php?id='.$id.'"><abbr title="Add extra IDs">➕</abbr></a>';
-                      echo '<a class="widget" href="' . $db_map_link . '"><abbr title="View on Database Map">🌎</abbr></a>';
-                      echo '<a class="widget" href="Edit.php?id='.$id.'"><abbr title="Edit">🔧</abbr></a>';
-                      echo '<a class="widget" href="Delete.php?id='.$id.'"><abbr title="Delete">✂️</abbr></a>';
-                      echo '<a class="widget" href="Reader.php?back_url=DB&id='.$id.'"><abbr title="View all info">🔍</abbr></a></td>';
-                      echo nl2br("<td class=" . "bio" . ">" . $bio . "</td>");
-                      }
-                    if(substr($evidence_link, 0, 14) == "image-evidence") $evidence_link = "uploads/$evidence_link";
+        // start widget echo
+        if($isMobile == "true"){
+          echo nl2br("<td class=" . "bio" . ">" . $bio);
+          if (!empty($bio)) echo '<br>';
+          echo '<div class="widget-td">';
+          echo '<a class="widget" href="SupplID.php?id='.$id.'"><abbr title="Add extra IDs">➕</abbr></a>';
+          echo '<a class="widget" href="' . $db_map_link . '"><abbr title="View on Database Map">🌎</abbr></a>';
+          echo '<a class="widget" href="Edit.php?id='.$id.'"><abbr title="Edit">🔧</abbr></a>';
+          echo '<a class="widget" href="Delete.php?id='.$id.'"><abbr title="Delete">✂️</abbr></a>';
+          echo '<a class="widget" href="Reader.php?back_url=DB&id='.$id.'"><abbr title="View all info">🔍</abbr></a></td>';
+          } else {
+          echo '<td class="widget-td" style="text-align: center;">';
+          echo '<a class="widget" href="SupplID.php?id='.$id.'"><abbr title="Add extra IDs">➕</abbr></a>';
+          echo '<a class="widget" href="' . $db_map_link . '"><abbr title="View on Database Map">🌎</abbr></a>';
+          echo '<a class="widget" href="Edit.php?id='.$id.'"><abbr title="Edit">🔧</abbr></a>';
+          echo '<a class="widget" href="Delete.php?id='.$id.'"><abbr title="Delete">✂️</abbr></a>';
+          echo '<a class="widget" href="Reader.php?back_url=DB&id='.$id.'"><abbr title="View all info">🔍</abbr></a></td>';
+          echo nl2br("<td class=" . "bio" . ">" . $bio . "</td>");
+          }
+        if(substr($evidence_link, 0, 14) == "image-evidence") $evidence_link = "uploads/$evidence_link";
 
-                    if (isset($evidence_link)) {
-                      if(substr($evidence_link, 0, 14) == "image-evidence") {
-                        if (file_exists($evidence_link)) {
-                        if($isMobile == "true") echo "<td class=" . "ev" . "><a target=" . "_blank" . " href=" . "$evidence_link" . ">Evidence</a>";
-                        if($isMobile != "true") echo "<td class=" . "ev" . "><a target=" . "_blank" . " href=" . "$evidence_link" . ">Evidence</a></td>";
-                      } else {
-                        if($isMobile == "true") echo "<td class=" . "ev" . ">Evidence is missing";
-                        if($isMobile != "true") echo "<td class=" . "ev" . ">Evidence is missing</td>";
-                      }
-                    } else {
-                      if($isMobile == "true") echo "<td class=" . "ev" . "><a target=" . "_blank" . " href=" . "$evidence_link" . ">Evidence</a>";
-                      if($isMobile != "true") echo "<td class=" . "ev" . "><a target=" . "_blank" . " href=" . "$evidence_link" . ">Evidence</a></td>";
-                    }
-                    } else {
-                    if($isMobile != "true") echo "<td class=" . "ev" . "></td>";
-                    }
+        if (isset($evidence_link)) {
+          if(substr($evidence_link, 0, 14) == "image-evidence") {
+            if (file_exists($evidence_link)) {
+            if($isMobile == "true") echo "<td class=" . "ev" . "><a target=" . "_blank" . " href=" . "$evidence_link" . ">Evidence</a>";
+            if($isMobile != "true") echo "<td class=" . "ev" . "><a target=" . "_blank" . " href=" . "$evidence_link" . ">Evidence</a></td>";
+          } else {
+            if($isMobile == "true") echo "<td class=" . "ev" . ">Evidence is missing";
+            if($isMobile != "true") echo "<td class=" . "ev" . ">Evidence is missing</td>";
+          }
+        } else {
+          if($isMobile == "true") echo "<td class=" . "ev" . "><a target=" . "_blank" . " href=" . "$evidence_link" . ">Evidence</a>";
+          if($isMobile != "true") echo "<td class=" . "ev" . "><a target=" . "_blank" . " href=" . "$evidence_link" . ">Evidence</a></td>";
+        }
+        } else {
+        if($isMobile != "true") echo "<td class=" . "ev" . "></td>";
+        }
+        include '../includes/functions/EV-math.php';
+        if($isMobile == "true") echo "<br>Score: " . $ev . "</td>";
+        if($isMobile != "true") echo "<td>" . $ev . "</td>";
+        echo "</tr>";
+      }
+      }
+      }
 
-                    if($isMobile == "true") echo "<br>Score: " . $evidence_score . "</td>";
-                    if($isMobile != "true") echo "<td>" . $evidence_score . "</td>";
-                  }
-            }
-echo "</tr>";
-
-    }
 ?>
 </tbody>
 </table>
