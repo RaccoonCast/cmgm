d007c42ca8cb02d479208955511d18b20b2d2934
Spane Boswell <spaneboswell101@gmail.com>
2021-03-22 19:15:44 -0700

    merge basic redirect into goto.php todo:// merge convert & goto

--- goto.php
+++ goto.php
@@ -1,17 +1,21 @@
 <?php
-if (!empty($_GET['latitude'])) { $latitude = $_GET['latitude']; }
-if (!empty($_GET['longitude'])) { $longitude = $_GET['longitude']; }
-if (!empty($_GET['carrier'])) { $carrier = $_GET['carrier']; }
-if (!empty($_GET['zip'])) { $zip = $_GET['zip']; }
-if (!empty($_GET['state'])) { $state = $_GET['state']; }
-if (!empty($_GET['city'])) { $city = $_GET['city']; }
-if (!empty($_GET['address'])) { $address = $_GET['address']; }
+foreach($_GET as $key => $value) ${$key} = $value;
 if (!empty($_GET['goto_page'])) { $goto_page = $_GET['goto_page']; } else {
   echo 'ERROR: $goto_page variable was not set.';
   die();
 }
-if ($goto_page == "CellMapper") $goto_page_URL = "basic-redirect.php?goto_page=cm&";
-if ($goto_page == "Google Maps") $goto_page_URL = "basic-redirect.php?goto_page=gm&";
+if ($goto_page == "CellMapper") {
+  $beginning = null;
+  if (!isset($carrier)) $carrier = null;
+  if ("$carrier" == "T-Mobile") $beginning = "MCC=310&MNC=260&";
+  if ("$carrier" == "Sprint") $beginning = "MCC=310&MNC=120&";
+  if ("$carrier" == "ATT") $beginning = "MCC=310&MNC=410&";
+  if ("$carrier" == "Verizon&") $beginning = "MCC=311&MNC=480&";
+  $the_URL = "https://www.cellmapper.net/map?$beginning" . "latitude=$latitude&longitude=$longitude&zoom=18&showTowerLabels=false";
+}
+if ($goto_page == "Google Maps") {
+ $the_URL = "https://www.google.com/maps/@?api=1&map_action=map&center=$latitude,$longitude&zoom=20&basemap=satellite";
+}
 if ($goto_page == "LA Permit Map") $goto_page_URL = "basic-redirect.php?goto_page=permit-map&";
 if ($goto_page == "Database") $goto_page_URL = "database/Home.php?";
 if ($goto_page == "Map") $goto_page_URL = "database/Map.php?";
@@ -25,15 +29,14 @@ if(empty($address)) $address = null;
 
 $suffix_part_b = '&address=' . $address . '&zip=' . $zip . '&city=' . $city .'&state=' . $state;
 $suffix_part_a = '&carrier=' . $carrier;
-
 if ($goto_page == "DB") { $suffix_part_a = null; $suffix_part_b = null; }
-if ($goto_page == "CellMapper") { $suffix_part_b = null; }
-if ($goto_page == "Google Maps") { $suffix_part_a = null; $suffix_part_b = null; }
 if ($goto_page == "Map") $suffix_part_b = null;
 
 if (isset($goto_page_URL)) {
 $the_URL = "$goto_page_URL" . "latitude=$latitude&longitude=$longitude" . "$suffix_part_a" . "$suffix_part_b";
 echo '<meta http-equiv="refresh" content="0; url=' . $the_URL . '">';
+} elseif(isset($the_URL)) {
+  echo '<meta http-equiv="refresh" content="0; url=' . $the_URL . '">';
 } else {
   echo 'ERROR: "' . $goto_page . '" does not have a specified redirect page.';
   die();
