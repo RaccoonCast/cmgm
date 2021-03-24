cede800ad1f8dfadea68ac9af7ac8c72eddfcf3e
Spane Boswell <spaneboswell101@gmail.com>
2021-03-24 10:54:31 -0700

    whoops; bat script will now auto-destruct

--- cmgm/includes/functions/sqlpw.php
+++ cmgm/includes/functions/sqlpw.php
@@ -3,7 +3,7 @@
 $servername = 'mysql.cmgm.gq';
 $db_username = 'cmgm';
 $siteroot = $_SERVER['DOCUMENT_ROOT'];
-$password = file_get_contents($siteroot . "/secret-sql-login.hiddenpass", true);
+$password = file_get_contents($siteroot . "\secret-sql-login.hiddenpass", true);
 $dbname = 'cmgm';
 $conn = mysqli_connect($servername, $db_username, $password, $dbname);
 ?>
