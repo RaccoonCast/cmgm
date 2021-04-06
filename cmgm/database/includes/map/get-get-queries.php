c58947a44d0b2cbbf5448e7a1acefb45187ebfe3
Spane Boswell <spaneboswell101@gmail.com>
2021-04-06 12:51:45 -0700

    add tag support to map page

--- cmgm/database/includes/map/get-get-queries.php
+++ cmgm/database/includes/map/get-get-queries.php
@@ -5,7 +5,9 @@ $url_suffix = null;
 foreach($_GET as $key => $value){
 
   if ($key == "latitude" OR $key == "longitude" OR $key == "address" OR $key == "zip" OR $key == "city" OR $key == "state" OR $key == "zoom" OR $key == "limit" OR $key == "marker_latitude" OR $key == "marker_longitude") {
-    ${$key} = $value;
+    ${$key} = $value; } elseif ($key == "tags") {
+    $url_suffix = $url_suffix . "&" . $key . "=" . $value;
+    $db_variables = $key . ' like "%'.$value.'%" AND ' . $db_variables;
   } elseif(!empty($value)) {
 
     if ($key != "id") {
