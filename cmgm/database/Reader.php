5889e265384c66ae4bb3999abbb1128469bba834
Spane Boswell <spaneboswell101@gmail.com>
2021-04-06 20:52:21 -0700

    this is a feature

--- cmgm/database/Reader.php
+++ cmgm/database/Reader.php
@@ -2,9 +2,19 @@
 $id = $_GET['id'];
 include "../includes/functions/sqlpw.php";
 include "../includes/functions/calculateEV.php";
-$back_url = $_GET['back_url'];
-if ($back_url == "Map-popup") $back_url = "Map-popup.php?id=" . $id;
-if ($back_url == "DB") $back_url = "DB.php#" . $id ;
+if ($_GET['show_empty_fields'] == 'true') {
+  $show_empty_fields = "true";
+} else {
+  $show_empty_fields = "false";
+}
+if (isset($_GET['back_url'])) {
+  $back = $_GET['back_url'];
+} else {
+  $back = "Home";
+}
+if ($back == "Map-popup") $back_url = "Map-popup.php?id=" . $id;
+if ($back == "DB") $back_url = "DB.php#" . $id ;
+if ($back == "Home") $back_url = "\Home.php";
 
 
 $sql = "SELECT * FROM database_db WHERE id = $id;";
@@ -12,7 +22,8 @@ $result = mysqli_query($conn,$sql);
 
 while($row = $result->fetch_assoc()) {
     foreach ($row as $key => $value) {
-        if (!empty($value)) {
+
+        if (!empty($value) OR $show_empty_fields == 'true') {
           if ($key != "evidence_score") {
           echo "$key" . ": " . $value;
           echo "<br>";
@@ -34,4 +45,11 @@ echo "new_evidence_score: " . $recalcEV . "";
 ?>
 <br>
 <br>
-<a href="<?php echo $back_url ?>">Back</a>
+<a href="<?php echo $back_url ?>">Back</a><br>
+<?php if ($show_empty_fields == 'false') { ?>
+  <a href="Reader.php?id=<?php echo $id?>&back_url=<?php echo $back?>&show_empty_fields=true">Show empty fields</a>
+<?php } else {
+  ?>
+    <a href="Reader.php?id=<?php echo $id?>&back_url=<?php echo $back?>&show_empty_fields=false">Don't show empty fields</a>
+  <?php
+} ?>
