f8e9052c3930fc4f31f7a64aae180a4b5ce20f18
Spane Boswell <spaneboswell101@gmail.com>
2021-04-10 11:27:07 -0700

    old code for useridsys that ive copied n pasted over but ye its better than the old code i copied n pasted in for temp

--- cmgm/includes/useridsys/edit.php
+++ cmgm/includes/useridsys/edit.php
@@ -1,22 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-   <head>
-	  <?php
-     if (isset($_GET['key'])) $key = $_GET['key'];
-     include "../functions.php"?>
-   </head>
-   <body>
-      <form action="google-maps.php" method="get" autocomplete="off">
-         <p>Google Maps API Key: </p>
-         <input type="text" name="key" id="txtresult" required>
-         <input type="submit" class="submitbutton" style="color: #00000;"  value="Submit">
-      </form>
-      <?php if (isset($_GET['key'])) { ?>
-      <script>
-      document.cookie = "api_key=<?php echo $key;?>; expires=Thu, 18 Dec 2030 12:00:00 UTC; path=/";
-      </script>
-       <?php
-     echo '<meta http-equiv="refresh" content="0;URL=../?refresh=true" /> ';
-   } ?>
-   </body>
-</html>
+<form action="create.php" method="post" autocomplete="off">
+   <p>Username: </p>
+   <input type="text" name="username" id="username" required>
+   <p>PIN: </p>
+   <input type="password" maxlength="4" name="pin" id="pin" required>
+   <p>Google Maps API Key: </p>
+   <input type="text" name="gmaps_api_key" id="gmaps_api_key">
+   <p>Latitude: </p>
+   <input type="text" name="default_latitude" id="default_latitude" required>
+   <p>Longitude: </p>
+   <input type="text" name="default_longitude" id="default_longitude" required>
+   <p>Carrier: </p>
+   <select class="custominput dropdown" autocomplete="on" name="default_carrier">
+     <option value="T-Mobile">T-Mobile</option>
+     <option value="ATT">AT&T</option>
+     <option value="Verizon">Verizon</option>
+     <option value="Sprint">Sprint</option>
+   </select>
+   <select class="custominput dropdown" autocomplete="on" name="theme">
+     <option value="black">AMOLED Black</option>
+     <option value="dark">Dark</option>
+     <option value="light">Light</option>
+     <option value="light">Original</option>
+   </select>
+   <input type="button" class="submitbutton" onclick="myFunction();" style="color: #00000;"  value="Locate">
+   <input type="submit" class="submitbutton" style="color: #00000;"  value="Submit">
+</form>
