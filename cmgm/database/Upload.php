6f042f1616fdd0aea21815101dc19e6b4aac6713
Spane Boswell <spaneboswell101@gmail.com>
2021-04-11 17:20:39 -0700

    Add some yet-to-work Ctrl+V code to upload, Update LFMF, "fix" sql, be an ocd hoe.

--- cmgm/database/Upload.php
+++ cmgm/database/Upload.php
@@ -1,11 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
+     <script src="https://code.jquery.com/jquery-latest.min.js"></script>
+     <script src="../js/pasteimages.js"></script>
       <?php
+      if (isset($_POST['type'])) $filenameType = $_POST['type'];
       include "../functions.php";
+      if (isset($_POST['base64_file'])) {
+        $siteroot = $_SERVER['DOCUMENT_ROOT'];
+        if ($siteroot == "/home/spane2003/cmgm.gq") {
+          define('UPLOAD_DIR', $siteroot . '/database/uploads/');
+        } else {
+          define('UPLOAD_DIR', $siteroot . '\database\uploads\\');
+        }
+        $img = $_POST['base64_file'];
+        $img = str_replace('data:image/png;base64,', '', $img);
+        $img = str_replace(' ', '+', $img);
+        $data = base64_decode($img);
+        $randomString = substr(str_shuffle(md5(time())),0,25);
+        $file = UPLOAD_DIR . $filenameType . '-' . $randomString . '.png';
+        $fileNoExtension = UPLOAD_DIR . $filenameType . '-' . $randomString;
+        $success = file_put_contents($file, $data);
 
-      if (isset($_POST['type'])) {
-          $filenameType = $_POST['type'];
+        $filePath = $file;
+        $image = imagecreatefrompng($filePath);
+        $bg = imagecreatetruecolor(imagesx($image), imagesy($image));
+        imagefill($bg, 0, 0, imagecolorallocate($bg, 255, 255, 255));
+        imagealphablending($bg, TRUE);
+        imagecopy($bg, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
+        imagedestroy($image);
+        $quality = 80; // 0 = worst / smaller file, 100 = better / bigger file
+        imagejpeg($bg, $fileNoExtension . ".jpg", $quality);
+        imagedestroy($bg);
+        unlink($file);
+        $evidence_a = $filenameType . '-' . $randomString . '.jpg';
+        echo $evidence_a;
+      } elseif (isset($_POST['type'])) {
           $target_dir = "uploads/";
           $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
           $uploadOk = 1;
@@ -44,6 +74,8 @@
    </head>
    <body>
       <form action="Upload.php" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="file" id="base64_file_form" />
+          <div id="picture" ></div>
         Select file to upload (Max: 10MB)<br><br>
         <select name="type" required>
           <option style="display:none" disabled selected="selected"></option>
