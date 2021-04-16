a320262fdf19157afea1b99b81e9ef98a2199f32
Spane Boswell <spaneboswell101@gmail.com>
2021-04-16 12:09:21 -0700

    re-working upload.php, massive hodge podge

--- cmgm/database/Upload.php
+++ cmgm/database/Upload.php
@@ -4,71 +4,19 @@
      <script src="https://code.jquery.com/jquery-latest.min.js"></script>
      <script src="../js/pasteimages.js"></script>
       <?php
-      if (isset($_POST['type'])) $filenameType = $_POST['type'];
       include "../functions.php";
+      if (isset($_POST['type'])) $filenameType = $_POST['type'];
+
+      if ($siteroot == "/home/spane2003/cmgm.gq") {
+        define('UPLOAD_DIR', $siteroot . '/database/uploads/');
+      } else {
+        define('UPLOAD_DIR', $siteroot . '\database\uploads\\');
+      }
+
       if (isset($_POST['base64_file'])) {
-        $siteroot = $_SERVER['DOCUMENT_ROOT'];
-        if ($siteroot == "/home/spane2003/cmgm.gq") {
-          define('UPLOAD_DIR', $siteroot . '/database/uploads/');
-        } else {
-          define('UPLOAD_DIR', $siteroot . '\database\uploads\\');
-        }
-        $img = $_POST['base64_file'];
-        $img = str_replace('data:image/png;base64,', '', $img);
-        $img = str_replace(' ', '+', $img);
-        $data = base64_decode($img);
-        $randomString = substr(str_shuffle(md5(time())),0,25);
-        $file = UPLOAD_DIR . $filenameType . '-' . $randomString . '.png';
-        $fileNoExtension = UPLOAD_DIR . $filenameType . '-' . $randomString;
-        $success = file_put_contents($file, $data);
-
-        $filePath = $file;
-        $image = imagecreatefrompng($filePath);
-        $bg = imagecreatetruecolor(imagesx($image), imagesy($image));
-        imagefill($bg, 0, 0, imagecolorallocate($bg, 255, 255, 255));
-        imagealphablending($bg, TRUE);
-        imagecopy($bg, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
-        imagedestroy($image);
-        $quality = 80; // 0 = worst / smaller file, 100 = better / bigger file
-        imagejpeg($bg, $fileNoExtension . ".jpg", $quality);
-        imagedestroy($bg);
-        unlink($file);
-        $evidence_a = $filenameType . '-' . $randomString . '.jpg';
-        echo $evidence_a;
+        include "includes/upload/ctrlv.php";
       } elseif (isset($_POST['type'])) {
-          $target_dir = "uploads/";
-          $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-          $uploadOk = 1;
-          $fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-          $filename = $filenameType . "-" . substr(str_shuffle(md5(time())),0,25);
-          $filePath = "$target_dir" . "$filename.$fileType";
-
-          // Check if file already exists
-          if (file_exists($target_file)) {
-            echo "Sorry, file already exists.";
-            $uploadOk = 0;
-          }
-
-          // Check file size
-          if ($_FILES["fileToUpload"]["size"] > 10000000) {
-            echo "Sorry, your file is too large.";
-            $uploadOk = 0;
-          }
-
-          // Check if $uploadOk is set to 0 by an error
-          if ($uploadOk == 1) {
-            $temp = explode(".", $_FILES["fileToUpload"]["name"]);
-            $newfilename = round(microtime(true)) . '.' . end($temp);
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $filePath)) {
-              ?>
-              <p onclick="copyToClipboard('<?php echo $filename; ?>.<?php echo $fileType; ?>')">It has been uploaded as <?php echo $filename?>.<?php echo $fileType?>, click me to copy.</p>
-              <?php
-              echo "<br>";
-            } else {
-              echo "Sorry, there was an error uploading your file.";
-            }
-          }
+        include "includes/upload/file-select.php";
         }
       ?>
    </head>
@@ -86,7 +34,13 @@
         </select>
         <input type="file" name="fileToUpload" id="fileToUpload" required>
         <input type="submit" value="Upload Image" name="submit">
-        <br><br><br><p>File name examples </p><hr>
+        <?php if (isset($finishedFilename)) { ?>
+          <p onclick="copyToClipboard('<?php echo $finishedFilename; ?>')">It has been uploaded as <?php echo $finishedFilename; ?>, click to copy.</p>
+        <?php } ?>
+
+        <!-- Misc -->
+
+        <br><br><br><br><br><br><br><p>File name examples </p><hr>
         <?php
         $randomString = $randomString = substr(str_shuffle(md5(time())),0,25);
         echo "image-evidence-" . $randomString . ".pdf";
---
+++ cmgm/database/includes/upload/file-select.php
@@ -0,0 +1,20 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+$filename = $filenameType . "-" . substr(str_shuffle(md5(time())),0,25);
+$filePath = "$target_dir" . "$filename.$fileType";
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 10000000) {
+  echo "Sorry, your file is too large.";
+  die();
+}
+
+$temp = explode(".", $_FILES["fileToUpload"]["name"]);
+$newfilename = round(microtime(true)) . '.' . end($temp);
+if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $filePath)) {
+$finishedFilename = $filename . "." . $fileType;
+}
+?>
