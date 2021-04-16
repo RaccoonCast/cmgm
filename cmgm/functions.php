b65f7aa7a1af380d0a3b76cac5d220bbfd84cc50
Spane Boswell <spaneboswell101@gmail.com>
2021-04-16 12:08:59 -0700

    Include copyToClipboard in functions.php

--- cmgm/functions.php
+++ cmgm/functions.php
@@ -4,6 +4,7 @@ include 'includes/useridsys/native.php';
 //include 'includes/allow_addr/checkIP.php';
 //include 'includes/useridsys/create.php';
 include 'includes/functions/basic-functions.php';
+?> <script src="/js/copyToClipboard.js"></script> <?php
 include 'includes/functions/css.php';
 include 'includes/functions/headhtml.php';
 include 'includes/functions/towerInfoGet.php';
