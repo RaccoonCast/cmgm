afb43631f8ec3c0f74da1fd57b22a1267e25e9fc
Spane Boswell <spaneboswell101@gmail.com>
2021-05-15 17:38:42 -0700

    Add testmap to homepage.

--- cmgm/goto.php
+++ cmgm/goto.php
@@ -1,5 +1,5 @@
 <?php
-if(empty($type)) $type = null;
+if(empty($type)) $type = "LTE";
 if(empty($carrier)) $carrier = null;
 if(empty($zip)) $zip = null;
 if(empty($city)) $city = null;
@@ -23,6 +23,17 @@ if ($goto_page == "CellMapper") {
   if ("$type" == "LTE") $beginning = $beginning . "type=LTE&";
   if ("$type" == "NR") $beginning = $beginning . "type=NR&";
   $the_URL = "https://www.cellmapper.net/map?$beginning" . "latitude=$latitude&longitude=$longitude&zoom=18&showTowerLabels=false";
+}
+if ($goto_page == "Testmap") {
+  $beginning = null;
+  if (!isset($carrier)) $carrier = null;
+  if ("$carrier" == "T-Mobile") $beginning = "310/260";
+  if ("$carrier" == "Sprint") $beginning = "310/120";
+  if ("$carrier" == "ATT") $beginning = "310/410";
+  if ("$carrier" == "Verizon") $beginning = "311/480";
+  if ("$type" == "LTE") $beginning = $beginning . "/LTE";
+  if ("$type" == "NR") $beginning = $beginning . "/NR";
+  $the_URL = "https://www.cellmapper.net/testmap/map/$beginning" . "?lat=$latitude&lng=$longitude&z=18";
 }
 if ($goto_page == "Google Maps") $the_URL = "https://www.google.com/maps/@?api=1&map_action=map&center=$latitude,$longitude&zoom=20&basemap=satellite";
 if ($goto_page == "Street View") $the_URL = "https://www.google.com/maps?layer=c&cbll=$latitude,$longitude";
