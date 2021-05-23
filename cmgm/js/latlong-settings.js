b430ee22908df50578e140b5b19c8518bb57b549
Spane Boswell <spaneboswell101@gmail.com>
2021-05-23 07:31:44 -0700

    whoops forgot this in https://github.com/RaccoonCast/cmgm/commit/91b81ffa9b00cbea9e3eb5e232767899e41613ce

--- cmgm/js/latlong-settings.js
+++ cmgm/js/latlong-settings.js
@@ -4,16 +4,11 @@ function myFunction() {
   }
 
   function showPosition(position) {
-    if (document.getElementById("latitude").value == "") {
       var latitude = position.coords.latitude;
       var longitude = position.coords.longitude;
-
+      console.log(latitude);
+      console.log(longitude);
       document.getElementById("latitude").value = latitude;
       document.getElementById("longitude").value = longitude;
-    }
   }
-}
-
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(showPosition);
 }
