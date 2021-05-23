81947fee15b2243b922c0759e5e893d704bd7a60
Spane Boswell <spaneboswell101@gmail.com>
2021-05-23 05:39:00 -0700

    Update LFMF-alt for evidence_c,attached_c.

--- cmgm/database/LFMF-alt.php
+++ cmgm/database/LFMF-alt.php
@@ -14,7 +14,7 @@ $fileList = glob('uploads/*.*');
 //Loop through the array that glob returned.
 foreach($fileList as $filename){
    $output = str_replace("uploads/", "", $filename);
-   $check = @mysqli_fetch_array(mysqli_query($conn, "SELECT id FROM database_db WHERE evidence_a='$output' OR evidence_b='$output' OR photo_a='$output' OR photo_b='$output' OR photo_c='$output' OR photo_d='$output' OR photo_e='$output' OR photo_f='$output' OR attached_a='$output' OR attached_b='$output'"))['id'];
+   $check = @mysqli_fetch_array(mysqli_query($conn, "SELECT id FROM database_db WHERE evidence_a='$output' OR evidence_b='$output' OR evidence_c='$output' OR photo_a='$output' OR photo_b='$output' OR photo_c='$output' OR photo_d='$output' OR photo_e='$output' OR photo_f='$output' OR attached_a='$output' OR attached_b='$output' OR attached_c='$output'"))['id'];
    if (!isset($check)) {
      // echo '<a href="uploads/' . $output . '">' . $output . '</a> does not exist in SQL DB<br>';
      unlink('uploads/' . $output . '');
