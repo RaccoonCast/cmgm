d7732608e0c08d04ec1548528e65746122657b9e
Spane Boswell <spaneboswell101@gmail.com>
2021-05-23 10:01:59 -0700

    Fix/add PWA.

--- cmgm/includes/functions/headhtml.php
+++ cmgm/includes/functions/headhtml.php
@@ -2,13 +2,20 @@
   <title>EvilCM - <?php echo $without_extension?></title>
 <?php } ?>
 <link rel="icon" type="image/png" href="/images/logo.png">
+<script type="module">
+import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+
+const el = document.createElement('pwa-update');
+document.body.appendChild(el);
+</script>
 <link rel='manifest' href='/manifest.json'>
 <link rel="apple-touch-icon" href="images/icons-192.png">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
 <meta charset="utf-8">
 <meta name="theme-color" content="#fff"/>
-<link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+
 <?php
 if ($_SERVER['SERVER_NAME'] == 'cmgm.gq') {
   $urlPrefix = "https://";
