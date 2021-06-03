dad25e3343da83529944655e80f2846512a8e1cd
Spane Boswell <spaneboswell101@gmail.com>
2021-06-03 13:14:09 -0700

    Avoid unneccesary reqs to google.

--- cmgm/includes/convert/get-address-for-loc.php
+++ cmgm/includes/convert/get-address-for-loc.php
@@ -1,5 +1,5 @@
 <?php
-if (isset($maps_api_key)) {
+if (isset($maps_api_key) && $goto_page != "CellMapper" && $goto_page != "Google Maps" && $goto_page != "Street View" && $goto_page != "Beta") {
 $url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='.trim($latitude).','.trim($longitude).'&key=' . $maps_api_key . '';
 $ch = curl_init(); curl_setopt($ch, CURLOPT_URL, $url); curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); curl_setopt($ch, CURLOPT_PROXYPORT, 3128); curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); $response = curl_exec($ch); curl_close($ch);
 mysqli_query($conn, "UPDATE userID SET gmaps_util = gmaps_util + 1 WHERE userIP = '$userIP'");
