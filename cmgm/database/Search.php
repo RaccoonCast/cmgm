10a7f1d32f844cc8162910a9c68c5fa52ac0425c
Spane Boswell <spaneboswell101@gmail.com>
2021-06-03 11:52:49 -0700

    Search UI revamp

--- cmgm/database/Search.php
+++ cmgm/database/Search.php
@@ -12,10 +12,9 @@
       <?php
          ?>
       <form action="DB.php" method="get">
-        <input type="hidden" name="latitude" value="<?php echo $latitude;?>">
-        <input type="hidden" name="longitude" value="<?php echo $longitude;?>">
-        <p>Cell site type is</p>
-        <select class="custominput cellsite-type-custom-width dropdown" name="cellsite_type">
+        <div class="buttons">
+          <h3>Search</h3><label>Cell Site Type</label><select
+        class="custominput cellsite-type-custom-width dropdown" name="cellsite_type">
         <option style="display:none" disabled selected="selected"></option>
         <option value="tower">Tower</option>
         <option value="rooftop">Rooftop</option>
@@ -28,20 +27,39 @@
         <option value="pole">Pole</option>
         <option value="disguised">Disguised structure</option>
         <option value="other">Other/Uknown</option>
+        </select><br><label>Concealment</label><select class="custominput" autocomplete="on" name="concealed" required>
+        <option style="display:none" disabled selected="selected"></option>
+        <option value="true">Concealed</option>
+        <option value="false">Unconcealed</option>
+        </select><br><label>Status</label><select
+        class="custominput cellsite-type-custom-width dropdown" name="status">
+        <option value="">All</option>
+        <option value="verified" selected="selected">Verified</option>
+        <option value="unverified">Unverified</option>
+        <option value="unmapped">Unmapped</option>
+        <option value="special">Special</option>
+        <option value="weird">Weird</option>
         </select>
-		       <p>the carrier is</p>
-           <select class="custominput dropdown" autocomplete="on" name="carrier">
+        </select>
+		       <br><label>Carrier</label><select
+              class="custominput dropdown" autocomplete="on" name="carrier">
              <option <?php if(empty($carrier)) echo 'selected="selected"';?> value=""></option>
              <option <?php if($carrier == "T-Mobile") echo 'selected="selected"';?> value="T-Mobile">T-Mobile</option>
              <option <?php if($carrier == "ATT") echo 'selected="selected"';?> value="ATT">AT&T</option>
              <option <?php if($carrier == "Verizon") echo 'selected="selected"';?> value="Verizon">Verizon</option>
              <option <?php if($carrier == "Sprint") echo 'selected="selected"';?> value="Sprint">Sprint</option>
           </select>
-		 <p>the LTE/NR id is</p>
-		 <input class="custominput" maxlength="30" name="id">
-		 <p>has file</p>
-		 <input class="custominput" maxlength="45" name="fileSearch">
-     <input type="submit" class="submitbutton" value="Submit">
+		 <br><label>LTE/NR id</label><input
+     class="custominput" maxlength="30" name="id">
+		 <br><label>File</label><input class="custominput small-text" maxlength="55" name="fileSearch">
+		 <br><label>City</label><input class="custominput" maxlength="45" name="city">
+		 <br><label>Zip</label><input class="custominput" maxlength="45" name="zip">
+		 <br><label>State</label><input class="custominput" maxlength="45" name="state">
+		 <br><label>Near lat/long</label><input class="latlong custominput" maxlength="45" value="<?php echo $latitude;?>" name="latitude"><input
+     class="latlong custominput" maxlength="45" value="<?php echo $longitude?>" name="longitude">
+     <br><label>Tag Search</label><input class="custominput" maxlength="45" name="tags">
+     <br><label></label><input type="submit" class="submitbutton custominput" value="Submit">
+   </div>
     </form>
     <?php
     // Get footer
