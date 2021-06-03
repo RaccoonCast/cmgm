f3b07e6812099d3b1938d6a5863b1e23e78d4f73
Spane Boswell <spaneboswell101@gmail.com>
2021-06-03 11:52:02 -0700

    ?noCheck for native.php

--- cmgm/functions.php
+++ cmgm/functions.php
@@ -2,7 +2,12 @@
 define ('SITE_ROOT', $_SERVER['DOCUMENT_ROOT']);
 $SITE_ROOT = $_SERVER['DOCUMENT_ROOT'];
 include 'includes/functions/basic-functions.php';
-include 'includes/useridsys/native.php';
+if (!isset($_GET['noCheck'])) {
+  include 'includes/useridsys/native.php';
+} else {
+  include SITE_ROOT . "/includes/functions/sqlpw.php";
+  $debug_flag = "off";
+}
 include 'includes/functions/getGetVars.php';
 include 'includes/functions/headhtml.php';
 // use \/ for testing lat,long reqs
