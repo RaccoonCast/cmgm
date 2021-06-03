95adb8c6b270fc2635630ded316aa6b7f7ecfdac
Spane Boswell <spaneboswell101@gmail.com>
2021-06-03 11:54:49 -0700

    Update Map-popup for new street view urls.

--- cmgm/database/Map-popup.php
+++ cmgm/database/Map-popup.php
@@ -12,7 +12,7 @@
 <?php
 $id = $_GET['mp-id'];
 
-$database_get_list = "id,date_added,LTE_1,LTE_2,LTE_3,LTE_4,LTE_5,LTE_6,carrier,latitude,longitude,city,zip,state,address,bio,evidence_score,evidence_a,street_view_url";
+$database_get_list = "id,date_added,LTE_1,LTE_2,LTE_3,LTE_4,LTE_5,LTE_6,carrier,latitude,longitude,city,zip,state,address,bio,evidence_score,evidence_a,street_view_url_a";
 
 $sql = "SELECT $database_get_list FROM database_db WHERE id = $id;";
 $result = mysqli_query($conn, $sql);
