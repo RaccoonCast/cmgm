ba239211330902d9d5ba9c07515d3e33c0e7e543
Spane Boswell <spaneboswell101@gmail.com>
2021-06-03 11:51:13 -0700

    "Fix" calcEV not having carrier specified.

--- cmgm/includes/functions/calculateEV.php
+++ cmgm/includes/functions/calculateEV.php
@@ -2,7 +2,7 @@
 function calculateEV($id,$carrier) {
 
 include "sqlpw.php";
-$db_variables = "id='$id'";
+$db_variables = "id='$id' AND carrier='$carrier'";
 $database_get_list = "permit_score,trails_match,carriers_dont_trail_match,antennas_match_carrier,cellmapper_triangulation,image_evidence,
 verified_by_visit,sector_split_match,archival_antenna_addition,only_reasonable_location,alt_carriers_here,other_user_map_primary";
 $sql = "SELECT $database_get_list FROM database_db WHERE $db_variables";
