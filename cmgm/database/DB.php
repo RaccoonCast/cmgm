fe97b9e41f3baeb31eba716ffe9bd93c0493d85e
Spane Boswell <spaneboswell101@gmail.com>
2021-06-09 00:48:54 -0700

    Fix #12

--- cmgm/database/DB.php
+++ cmgm/database/DB.php
@@ -66,7 +66,7 @@ while($row = $result->fetch_assoc()) {
 
         if(substr($evidence_a, 0, 14) == "image-evidence") $evidence_a = "uploads/$evidence_a";
 
-        if (isset($evidence_a)) {
+        if (!empty($evidence_a)) {
           if(substr($evidence_a, 0, 14) == "image-evidence") {
             if (file_exists($evidence_a)) {
             if($isMobile == "true") echo "<td class=" . "ev" . "><a target=" . "_blank" . " href=" . "$evidence_a" . ">Evidence</a>";
