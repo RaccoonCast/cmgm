0701e47209fb4e38bc61e928bfd5ee34c8051a97
Spane Boswell <spaneboswell101@gmail.com>
2021-06-08 17:14:39 -0700

    getGetVars runs BEFORE native so it won't have $default_carrier set

--- cmgm/includes/functions/getGetVars.php
+++ cmgm/includes/functions/getGetVars.php
@@ -13,7 +13,7 @@ if (!empty($_GET['city'])) { $city = $_GET['city']; }
 if (!empty($_GET['address'])) { $address = $_GET['address']; }
 if (!empty($_GET['data'])) { $data = $_GET['data']; }
 if (!empty($_GET['conv_type'])) { $conv_type = $_GET['conv_type']; }
-if (!isset($carrier)) if (isset($default_carrier)) { $carrier = $default_carrier; } else { $default_carrier = "ATT"; $carrier = $default_carrier; }
+if (!isset($carrier)) if (isset($default_carrier)) { $carrier = $default_carrier; } 
 
 // get latitude from POST
 if (!empty($_POST['latitude'])) { $latitude = $_POST['latitude']; }
