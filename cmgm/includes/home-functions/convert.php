9d0794fd26dc807bfe658b6c9dd238560dbb6375
Spane Boswell <spaneboswell101@gmail.com>
2021-06-08 17:14:19 -0700

    Tweak convert

--- cmgm/includes/home-functions/convert.php
+++ cmgm/includes/home-functions/convert.php
@@ -14,13 +14,18 @@ if ($data == "defaultLoc") {
 
 if (!isset($conv_type)) {
   // CellMapper URL Conversion
+  
   if(substr("$data", 0, 26 === 'https://www.cellmapper.net') && !isset($conv_type)) include "includes/convert/cellmapper.net.php";
   // Google Maps URL Conversion
   elseif(substr("$data", 0, 28 === 'https://www.google.com/maps/') && !isset($conv_type)) include "includes/convert/google-maps-url-conversion.php";
   // Comma Seperator
   elseif(strpos($data, ',') !== false && !isset($conv_type)) include "includes/convert/lat,long.php";
   // NOTHING? Google Maps search for the entered data // GET ADDRESS INFO FOR LOCATION
-  include 'includes/convert/google-maps-conversion.php';
+  include SITE_ROOT . '/includes/convert/google-maps-conversion.php';
+  // DMS TO DEC
+  if (!isset($conv_type)) include "includes/convert/dmstodec.php";
+  // Get address info for location
+  include SITE_ROOT . "/includes/convert/get-address-for-loc.php";
 
   $latitude = substr($latitude,0,10);
   $longitude = substr($longitude,0,10);
