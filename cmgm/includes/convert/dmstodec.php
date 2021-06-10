a5f636c90f88a48a7c5b3583a6071c88396ba5e5
Spane Boswell <spaneboswell101@gmail.com>
2021-06-09 23:31:47 -0700

    10,000% better DMS to DEC code

--- cmgm/includes/convert/dmstodec.php
+++ cmgm/includes/convert/dmstodec.php
@@ -1,48 +1,14 @@
 <?php
-//if latitude and longitude still not set try DMS to LatLong conversion
 function DMStoDEC($deg,$min,$sec) {
   return $deg+((($min*60)+($sec))/3600);
 }
-if (substr("$data", 11, 1) === 'N') {
-$data = str_replace(',', '', $data);
-// 40-40-54.4 N (40) (40) (54.4) (N) AND 34-05-50.9 N (34) (05) (50.9) (N)
-$deg1 = substr($data,0,2);
-$min1 = substr($data,3,2);
-$sec1 = substr($data,6,4);
-
-// 73-59-36.3 W (77) (59) (36.3) (W) AND 118-15-15.7 W (118) (15) (51.7) (W)
-// check for 3 digit $deg2
-if (substr("$data", 16, 1) === '-') {
-  $deg2 = substr($data,13,3);
-  $min2 = substr($data,17,2);
-  $sec2 = substr($data,20,4);
-} else {
-  $deg2 = substr($data,13,2);
-  $min2 = substr($data,16,2);
-  $sec2 = substr($data,19,4);
+preg_match_all('!\d+\.*\d*!', $data, $out);
+$latitude = DMStoDec($out[0][0],$out[0][1],$out[0][2]);
+$longitude = DMStoDec($out[0][3],$out[0][4],$out[0][5]);
+if (!is_numeric(@$latitude) OR !is_numeric(@$longitude)) {
+  $conv_type = "DMS Coordinates";
 }
-} else {
-  // 40-40-54.4N (40) (40) (54.4) (N) AND 34-05-50.9N (34) (05) (50.9) (N)
-  $deg1 = substr($data,0,2);
-  $min1 = substr($data,3,2);
-  $sec1 = substr($data,6,4);
-
-  // 73-59-36.3W (77) (59) (36.3) (W) AND 118-15-15.7W (118) (15) (51.7) (W)
-  // check for 3 digit $deg2
-  if (substr("$data", 15, 1) === '-') {
-    $deg2 = substr($data,12,3);
-    $min2 = substr($data,16,2);
-    $sec2 = substr($data,19,4);
-  } else {
-    $deg2 = substr($data,13,2);
-    $min2 = substr($data,16,2);
-    $sec2 = substr($data,19,4);
-  }
-}
-$latitude = DMStoDec($deg1,$min1,$sec1);
-$longitude = DMStoDec($deg2,$min2,$sec2);
 if(strpos($data, "S") !== false) $latitude = "-".$latitude;
 if(strpos($data, "W") !== false) $longitude = "-".$longitude;
 
-$conv_type = "DMS Coordinates";
 ?>
