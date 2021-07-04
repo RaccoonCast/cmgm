7d910e36ebdcb8dfcd558a80ee4fd4a1d921e176
Spane Boswell <spaneboswell101@gmail.com>
2021-07-04 06:35:05 -0700

    add some well deserved padding

--- cmgm/database/includes/edit/the-form.php
+++ cmgm/database/includes/edit/the-form.php
@@ -63,12 +63,12 @@
     <label class="id_params_label">Sector matches</label><input type="text" class="id_params_cw sector_match" name="sector_match" value="<?php echo @$sector_match?>">
     <label class="id_params_label">Primary already located</label><input type="text" class="id_params_cw other_user_map_primary" name="other_user_map_primary" value="<?php echo @$other_user_map_primary?>">
 
-    <label class="latitude_longitude_label" for="latitude"><span style="float: right"><a href="#" onclick="copyToClipboard('<?php echo $latitude . ","; ?><?php echo $longitude; ?>')">Copy</a></span><a target="_blank" href="<?php echo @$cellmapper_link_lte;?>">Latitude/Longitude</a></label><?php if ($isMobile =="true") { ?><br><?php } ?><input
+    <label class="latitude_longitude_label" for="latitude"><span style="float: right"><a href="#" class="pad-small-link" onclick="copyToClipboard('<?php echo $latitude . ","; ?><?php echo $longitude; ?>')">Copy</a></span><a target="_blank" href="<?php echo @$cellmapper_link_lte;?>">Latitude/Longitude</a></label><?php if ($isMobile =="true") { ?><br><?php } ?><input
     type="text" class="inline-block latitude_longitude_cw" id="latitude" value="<?php echo @$latitude?>" placeholder="Latitude" name="latitude"><input
     type="text" class="inline-block latitude_longitude_cw" id="longitude" value="<?php echo @$longitude?>" placeholder="Longitude" name="longitude">
 
     <label class="addr_label" for="address"><a target="_blank" href="https://www.google.com/maps/@?api=1&map_action=map&center=<?php echo @$latitude; ?>,<?php echo @$longitude; ?>&zoom=20&basemap=satellite">Address</a>
-    <span style="float: right"><a href="#" onclick="copyToClipboard('<?php echo $address . ", " . $city . ", " . $state . " " . $zip; ?>')">Copy</a></label><?php if ($isMobile =="true") { ?><br><?php } ?><input
+    <span style="float: right"><a href="#" class="pad-small-link" onclick="copyToClipboard('<?php echo $address . ", " . $city . ", " . $state . " " . $zip; ?>')">Copy</a></label><?php if ($isMobile =="true") { ?><br><?php } ?><input
     type="text" autocomplete="chrome-off" class="inline-block addr_address_cw" id="address" value="<?php echo @$address?>" placeholder="Address" name="address"><input
     type="text" autocomplete="chrome-off" class="inline-block addr_city_cw" id="city" value="<?php echo @$city?>" placeholder="City" name="city"><input
     type="text" autocomplete="chrome-off" class="inline-block addr_state_cw" id="state" value="<?php echo @$state?>" placeholder="State" name="state"><input
