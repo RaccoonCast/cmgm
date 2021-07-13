1e7c16df075a4c8c20548e258e7c8cbb1b7639a0
Spane Boswell <spaneboswell101@gmail.com>
2021-07-12 20:20:50 -0700

    More advanced search for Edit.php.

--- cmgm/database/includes/edit/missing_id.php
+++ cmgm/database/includes/edit/missing_id.php
@@ -1,14 +1,18 @@
 <?php
-if (!isset($_GET['new'])) {
-   if (@$back_num < 15 & isset($back_num)) redir("Edit.php?id=" . --$id . "&back=" . ++$back_num . "","0"); // back
-   if (@$next_num < 15 & isset($next_num)) redir("Edit.php?id=" . ++$id . "&next=" . ++$next_num . "","0"); // next
-   if (!empty($id)) $new_id = @mysqli_fetch_array(mysqli_query($conn, "SELECT id FROM database_db WHERE LTE_1='$id' OR LTE_2='$id' OR LTE_3='$id' OR LTE_4='$id' OR LTE_5='$id' OR LTE_5='$id' OR LTE_6='$id' OR NR_1='$id' OR NR_2='$id'"))['id']; // id search
-   if (!empty($new_id)) redir("Edit.php?id=$new_id","0"); // redir to searched ID
-   ?> <title>EvilCM - Edit</title> <?php
-   include "includes/edit/id_input_1.php";
-   include "includes/edit/prev_next.php";
-   die();
+if (isset($_GET['new'])) {
+     $id = mysqli_fetch_array(mysqli_query($conn, "SELECT t1.id + 1 FROM database_db t1 WHERE NOT EXISTS (SELECT * FROM database_db t2 WHERE t2.id = t1.id + 1) LIMIT 1"))['t1.id + 1'];
 } else {
-   $id = mysqli_fetch_array(mysqli_query($conn, "SELECT t1.id + 1 FROM database_db t1 WHERE NOT EXISTS (SELECT * FROM database_db t2 WHERE t2.id = t1.id + 1) LIMIT 1"))['t1.id + 1'];
+  if (@$back_num < 15 & isset($back_num)) redir("Edit.php?id=" . --$id . "&back=" . ++$back_num . "","0"); // back
+  if (@$next_num < 15 & isset($next_num)) redir("Edit.php?id=" . ++$id . "&next=" . ++$next_num . "","0"); // next
+  if (!empty($id)) $new_id = @mysqli_fetch_array(mysqli_query($conn, "SELECT id FROM database_db WHERE LTE_1='$id' OR LTE_2='$id' OR LTE_3='$id' OR LTE_4='$id' OR LTE_5='$id' OR LTE_5='$id' OR LTE_6='$id' OR NR_1='$id' OR NR_2='$id'"))['id']; // id search
+  if (!empty($new_id)) redir("Edit.php?id=$new_id","0"); // redir to searched ID
+  ?> <title>EvilCM - Edit</title> <?php
+  // LAST DITCH EFFORT TO FIGURE OUT WHO WE EDITING
+  include SITE_ROOT . "/includes/home-functions/convert.php";
+  if (!empty($id)) [$latitude,$longitude] = convert($id,"HomeSmart",$default_latitude,$default_longitude,$maps_api_key,$userIP,$default_carrier);
+  if (isset($latitude) && isset($longitude)) redir("Edit.php?id=" . @mysqli_fetch_array(mysqli_query($conn,"SELECT DISTINCT id, (3959 * ACOS(COS(RADIANS($latitude)) * COS(RADIANS(latitude)) * COS(RADIANS(longitude) - RADIANS($longitude)) + SIN(RADIANS($latitude)) * SIN(RADIANS(latitude)))) AS DISTANCE FROM database_db ORDER BY distance LIMIT 1"))['id'],"0"); // redir to searched ID
+  include "includes/edit/id_input_1.php";
+  include "includes/edit/prev_next.php";
+  die();
 }
 ?>
--- cmgm/database/includes/edit/read_data.php
+++ cmgm/database/includes/edit/read_data.php
@@ -3,5 +3,5 @@ $sql_read = "SELECT ";
 foreach ($list_of_vars as $value) $sql_read = $sql_read . $value . ",";
 $sql_read = rtrim($sql_read, ',') . " FROM database_db WHERE id = $id;";
 $sql_read_result = mysqli_query($conn,$sql_read);
-if (!empty($id)) while($row = $sql_read_result->fetch_assoc()) foreach ($row as $key => $value) $$key = $value;
+if (!empty($id)) if (is_numeric($id)) while($row = $sql_read_result->fetch_assoc()) foreach ($row as $key => $value) $$key = $value;
 ?>
