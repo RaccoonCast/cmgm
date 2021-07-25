fdbac9a9fec67c8ca46e5fe2cf81705fa81c53b8
Spane Boswell <spaneboswell101@gmail.com>
2021-07-24 17:51:08 -0700

    Decrease icon size of Map pins on mobile.

--- cmgm/database/includes/map/iconsize-mobile.php
+++ cmgm/database/includes/map/iconsize-mobile.php
@@ -1,29 +1,29 @@
 var unverified = L.icon({
     iconUrl: '../images/red.png',
-    iconSize: [16, 16], // size of the icon
+    iconSize: [15, 15], // size of the icon
     popupAnchor: [0, -15]
 });
 
 var verified = L.icon({
     iconUrl: '../images/green.png',
-    iconSize: [16, 16], // size of the icon
+    iconSize: [15, 15], // size of the icon
     popupAnchor: [0, -15]
 });
 
 var special = L.icon({
     iconUrl: '../images/purple.png',
-    iconSize: [16, 16], // size of the icon
+    iconSize: [15, 15], // size of the icon
     popupAnchor: [0, -15]
 });
 
 var weird = L.icon({
     iconUrl: '../images/blue.png',
-    iconSize: [16, 16], // size of the icon
+    iconSize: [15, 15], // size of the icon
     popupAnchor: [0, -15]
 });
 
 var unmapped = L.icon({
     iconUrl: '../images/white.png',
-    iconSize: [16, 16], // size of the icon
+    iconSize: [15, 15], // size of the icon
     popupAnchor: [0, -15]
 });
