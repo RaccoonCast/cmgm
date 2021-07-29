81c04106a8a8bb2ea6d095ead2ef6574c3c2fcf4
Spane Boswell <spaneboswell101@gmail.com>
2021-07-29 11:49:52 -0700

    miiiiiniiiiiiiiiiifyyyyyyyyyyyyyyyyy id-list

--- cmgm/includes/misc-functions/id-list.php
+++ cmgm/includes/misc-functions/id-list.php
@@ -1,39 +1,8 @@
 <?php
-if (file_exists("id_list.txt")) {
-  unlink("id_list.txt");
-}
-
+include "../../database/includes/DB-filter.php";
 include '../functions/sqlpw.php';
-include '../functions/basic-functions.php';
 
-$sql = "SELECT LTE_1,LTE_2,LTE_3,LTE_4,LTE_5,LTE_6,NR_1,NR_2 FROM database_db;";
+$sql = "SELECT DISTINCT LTE_1,LTE_2,LTE_3,LTE_4,LTE_5,LTE_6,NR_1,NR_2, (3959 * ACOS(COS(RADIANS($latitude)) * COS(RADIANS(latitude)) * COS(RADIANS(longitude) - RADIANS($longitude)) + SIN(RADIANS($latitude)) * SIN(RADIANS(latitude)))) AS DISTANCE FROM database_db ".@$db_vars." ORDER BY distance";
 $result = mysqli_query($conn, $sql);
-while ($row = mysqli_fetch_assoc($result)) {
-
- $colCount = 1;
-   foreach ($row as $field => $value) {
-     $sepCount = ($colCount++);
-
-switch ($sepCount) {
- case 1:  $LTE_1 = $value; break;
- case 2:  $LTE_2 = $value; break;
- case 3:  $LTE_3 = $value; break;
- case 4:  $LTE_4 = $value; break;
- case 5:  $LTE_5 = $value; break;
- case 6:  $LTE_6 = $value; break;
- case 7:  $NR_1 = $value; break;
- case 8:  $NR_2 = $value;
-
-if (!empty($LTE_1)) file_put_contents('id_list.txt', $LTE_1."\n",FILE_APPEND);
-if (!empty($LTE_2)) file_put_contents('id_list.txt', $LTE_2."\n",FILE_APPEND);
-if (!empty($LTE_3)) file_put_contents('id_list.txt', $LTE_3."\n",FILE_APPEND);
-if (!empty($LTE_4)) file_put_contents('id_list.txt', $LTE_4."\n",FILE_APPEND);
-if (!empty($LTE_5)) file_put_contents('id_list.txt', $LTE_5."\n",FILE_APPEND);
-if (!empty($LTE_6)) file_put_contents('id_list.txt', $LTE_6."\n",FILE_APPEND);
-if (!empty($NR_1)) file_put_contents('id_list.txt', $NR_1."\n",FILE_APPEND);
-if (!empty($NR_2)) file_put_contents('id_list.txt', $NR_2."\n",FILE_APPEND);
-break;
-           }
-   }
-}
+while ($row = mysqli_fetch_assoc($result)) foreach ($row as $field => $value) if (strlen($value) <= 7 && !empty($value)) echo $value , PHP_EOL
 ?>
