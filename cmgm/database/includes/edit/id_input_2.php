df4df504b153560e72560a2a394edcdcdadb7610
Spane Boswell <spaneboswell101@gmail.com>
2021-08-28 02:29:49 -0700

    code is in, now it just has to be coded to actually work.

--- cmgm/database/includes/edit/id_input_2.php
+++ cmgm/database/includes/edit/id_input_2.php
@@ -1,4 +1,4 @@
-Or...
-<form style="display:inline" action="Edit.php" autocomplete="off" method="get">
-<input type="text" style="width: 110px; height: 36px" value="" placeholder="Any LTE/NR ID" name="id"><input type="submit" style="height: 36px" value="Submit">
+Or edit instead
+<form style="display:inline" action="Edit.php" autocomplete="off" method="get" required>
+<input type="text" style="width: 110px; height: 36px" value="" placeholder="" name="id"><input type="submit" style="height: 36px" name="loctype" value="ID Search"><input type="submit" style="height: 36px" name="loctype" value="Location Search">
 </form>
