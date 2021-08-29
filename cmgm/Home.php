90253fffc432d945a17a1d2af1dd847c00972be6
Spane Boswell <spaneboswell101@gmail.com>
2021-08-29 09:52:43 -0700

    yay more complicated - just waht i wanted

--- cmgm/Home.php
+++ cmgm/Home.php
@@ -26,18 +26,18 @@
          <input type="hidden" name="city" value="<?php echo $city;?>">
          <input type="hidden" name="state" value="<?php echo $state;?>">
          <input type="hidden" id="rerunData" name="rerunData" value="false"><input
-         type="submit" class="sb w-33" style="color:#D93A6C" id="link01" name="goto" onclick="changeF('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"DB-Form",NULL) ?>');"value="DB-Form"><input
-         type="submit" class="sb w-33" style="color:#D93A6C" id="link02" name="goto" onclick="changeF('/database/Edit.php?id=<?php echo $latitude . "," . $longitude;?>);" value="DB-Edit"><input
-         type="submit" class="sb w-33" style="color:#D93A6C" id="link03" name="goto" onclick="changeF('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"DB-Map",NULL) ?>');" value="DB-Map"><input
-         type="submit" class="sb w-50" style="color:#E31BDC" id="link04" name="goto" onclick="changeF('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"Search",NULL) ?>');" value="Search"><input
-         type="submit" class="sb w-50" style="color:#E9A623" id="link05" name="goto" onclick="changeF('database/Upload.php');" name="Upload" value="Upload"><input
-         type="submit" class="sb w-50" style="color:#33D333" id="link06" name="goto" onclick="changeF('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"CellMapper",NULL) ?>');" value="CellMapper"><input
-         type="submit" class="sb w-50" style="color:#33D333" id="link07" name="goto" onclick="changeF('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"Beta",NULL) ?>');" value="Beta"><input
-         type="submit" class="sb w-50" style="color:#5695F6" id="link08" name="goto" onclick="redir('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"Google Maps",NULL) ?>');" value="Google Maps"><input
-         type="submit" class="sb w-50" style="color:#5695F6" id="link09" name="goto" onclick="redir('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"Street View",NULL) ?>');" value="Street View"><input
-         type="submit" class="sb w-50" style="color:#6BE63E" id="link10" name="goto" onclick="changeF('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"Permits",NULL) ?>');" value="Permits"><input
-         type="submit" class="sb w-50" style="color:#6BE63E" id="link11" name="goto" onclick="changeF('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"AntennaSearch",NULL) ?>');" value="AntennaSearch"><input
-         type="submit" class="sb w-100" style="color:#BA03FC" id="link12" name="goto" onclick="changeF('includes/useridsys/Settings.php');" value="Settings">
+         type="button" class="sb w-33" style="color:#D93A6C" id="link01" name="goto" onclick="redir('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"DB-Form",NULL) ?>');"value="DB-Form"><input
+         type="button" class="sb w-33" style="color:#D93A6C" id="link02" name="goto" onclick="redir('/database/Edit.php?id=<?php echo $latitude . "," . $longitude;?>');" value="DB-Edit"><input
+         type="button" class="sb w-33" style="color:#D93A6C" id="link03" name="goto" onclick="redir('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"DB-Map",NULL) ?>');" value="DB-Map"><input
+         type="button" class="sb w-50" style="color:#E31BDC" id="link04" name="goto" onclick="redir('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"Search",NULL) ?>');" value="Search"><input
+         type="button" class="sb w-50" style="color:#E9A623" id="link05" name="goto" onclick="redir('database/Upload.php');" name="Upload" value="Upload"><input
+         type="button" class="sb w-50" style="color:#33D333" id="link06" name="goto" onclick="redir('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"CellMapper",NULL) ?>');" value="CellMapper"><input
+         type="button" class="sb w-50" style="color:#33D333" id="link07" name="goto" onclick="redir('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"Beta",NULL) ?>');" value="Beta"><input
+         type="button" class="sb w-50" style="color:#5695F6" id="link08" name="goto" onclick="redir('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"Google Maps",NULL) ?>');" value="Google Maps"><input
+         type="button" class="sb w-50" style="color:#5695F6" id="link09" name="goto" onclick="redir('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"Street View",NULL) ?>');" value="Street View"><input
+         type="button" class="sb w-50" style="color:#6BE63E" id="link10" name="goto" onclick="redir('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"Permits",NULL) ?>');" value="Permits"><input
+         type="button" class="sb w-50" style="color:#6BE63E" id="link11" name="goto" onclick="redir('<?php echo function_goto($latitude,$longitude,@$carrier,@$address,@$zip,@$city,@$state,"AntennaSearch",NULL) ?>');" value="AntennaSearch"><input
+         type="button" class="sb w-100" style="color:#BA03FC" id="link12" name="goto" onclick="redir('includes/useridsys/Settings.php');" value="Settings">
      </form>
    </body>
 </html>
--- cmgm/js/index.js.php
+++ cmgm/js/index.js.php
@@ -2,15 +2,34 @@
 <script>
 function changeFormAction() {
   document.getElementById("link01").removeAttribute("onclick");
+  document.getElementById("link01").setAttribute('type', 'submit');
+
   document.getElementById("link02").removeAttribute("onclick");
+  document.getElementById("link02").setAttribute('type', 'submit');
+
   document.getElementById("link03").removeAttribute("onclick");
+  document.getElementById("link03").setAttribute('type', 'submit');
+
   document.getElementById("link04").removeAttribute("onclick");
+  document.getElementById("link04").setAttribute('type', 'submit');
+
   document.getElementById("link06").removeAttribute("onclick");
+  document.getElementById("link06").setAttribute('type', 'submit');
+
   document.getElementById("link07").removeAttribute("onclick");
+  document.getElementById("link07").setAttribute('type', 'submit');
+
   document.getElementById("link08").removeAttribute("onclick");
+  document.getElementById("link08").setAttribute('type', 'submit');
+
   document.getElementById("link09").removeAttribute("onclick");
+  document.getElementById("link09").setAttribute('type', 'submit');
+
   document.getElementById("link10").removeAttribute("onclick");
+  document.getElementById("link10").setAttribute('type', 'submit');
+  
   document.getElementById("link11").removeAttribute("onclick");
+  document.getElementById("link11").setAttribute('type', 'submit');
 }
 function changeF(redirect) {
   document.getElementById("form").setAttribute('action', redirect);
