60e3b212cdfb90627fb2ecd33ffecd34efb24f62
Spane Boswell <spaneboswell101@gmail.com>
2021-09-24 16:28:11 -0700

    date before/after feature, bug fix. (DB-Filter)

--- cmgm/database/includes/DB-filter.php
+++ cmgm/database/includes/DB-filter.php
@@ -1,28 +1,33 @@
 <?php
 foreach($_GET as $key => $value){
-@$id = str_replace(' ', '', $value);
-@$fs = str_replace(' ', '', $value);
+@$fs = @$id = str_replace(' ', '', $value);
 @$trimChar = substr($value, 1);
 
 if (!empty($value) OR $value == "NULL") {
-    if ($key != "latitude" && $key != "longitude" && $key != "zoom" && $key != "mp-id") @$url_suffix = @$url_suffix . "&" . $key . "=" . $value;
-    if ($value == "NULL") $value = null;
-    if ($key == "latitude" OR $key == "longitude" OR $key == "zoom" OR $key == "limit" OR $key == "marker_latitude" OR $key == "marker_longitude" OR $key == "back") { ${$key} = $value; }
-    elseif ($key == "date" && $value[0] == ">") { $db_vars = "AND date_added" . ' >= "'.date("Y-m-d", strtotime($trimChar)).'"' . @$db_vars; }
-    elseif ($key == "date" && $value[0] == "<") { $db_vars = "AND date_added" . ' <= "'.date("Y-m-d", strtotime($trimChar)).'"' . @$db_vars; }
-    elseif ($key == "date") { $db_vars = "AND date_added" . ' = "'.date("Y-m-d", strtotime($value)).'"' . @$db_vars; }
-    elseif ($key == "has_street_view" && $value == "true") $db_vars = " AND street_view_url_a != '' " . @$db_vars;
-    elseif ($key == "has_street_view" && $value == "false") $db_vars = " AND street_view_url_a = '' " . @$db_vars;
-    elseif ($value[0] == "!") { $db_vars = " AND NOT " . $key . ' = "'.$trimChar.'"' . @$db_vars; }
-    elseif ($value[0] == ">") { $db_vars = " AND ". $key . ' > '.$trimChar . @$db_vars; }
-    elseif ($value[0] == "<") { $db_vars = " AND ". $key . ' < '.$trimChar . @$db_vars; }
-    elseif ($key == "idlist") { $db_vars = " AND FIND_IN_SET(`id`, '$value')" . @$db_vars; }
-    elseif ($key == "fileSearch") { $db_vars = " AND evidence_a like '%$fs%' OR evidence_b like '%$fs%' OR evidence_c like '%$fs%' OR photo_a like '%$fs%' OR photo_b like '%$fs%' OR photo_c like '%$fs%' OR photo_d like '%$fs%' OR photo_e like '%$fs%' OR photo_f like '%$fs%' OR extra_a like '%$fs%' OR extra_b like '%$fs%' OR extra_c like '%$fs%'" . @$db_vars; }
-    elseif ($key == "address") { $db_vars = " AND " . $key . ' like "%'.$value.'%"' . @$db_vars; }
-    elseif ($key == "tags") { $db_vars = " AND ". $key . ' like "%'.$value.'%"' . @$db_vars; }
-    elseif ($key == "id") { $db_vars = "AND (id = '$id' OR LTE_1 = '$id' OR LTE_2 = '$id' OR LTE_3 = '$id' OR LTE_4 = '$id' OR LTE_5 = '$id' OR LTE_6 = '$id' OR NR_1 = '$id' OR NR_2 = '$id') " . @$db_vars; }
-    else { $db_vars = " AND ". $key . ' = "'.$value.'"' . @$db_vars; }
-  }
+  if ($key != "latitude" && $key != "longitude" && $key != "zoom" && $key != "mp-id") @$url_suffix = @$url_suffix . "&" . $key . "=" . $value;
+  if ($value == "NULL") $value = null;
+  if ($key == "latitude" OR $key == "longitude" OR $key == "zoom" OR $key == "limit" OR $key == "marker_latitude" OR $key == "marker_longitude" OR $key == "back") { ${$key} = $value; }
+  elseif ($key == "id" AND (strpos($value, '-') !== false)) { $strings = explode('-',$value); $db_vars = " AND ID BETWEEN $strings[0] AND $strings[1]" . @$db_vars; }
+  elseif ($key == "date" AND (strpos($value, ',') !== false)) {
+      $strings = explode(',',$value);
+      $db_vars = " AND date_added" . ' >= "'.date("Y-m-d", strtotime($strings[0])).'"' . @$db_vars;
+      $db_vars = " AND date_added" . ' <= "'.date("Y-m-d", strtotime($strings[1])).'"' . @$db_vars;
+    }
+  elseif ($key == "id") { $db_vars = "AND (id = '$id' OR LTE_1 = '$id' OR LTE_2 = '$id' OR LTE_3 = '$id' OR LTE_4 = '$id' OR LTE_5 = '$id' OR LTE_6 = '$id' OR NR_1 = '$id' OR NR_2 = '$id') " . @$db_vars; }
+  elseif ($key == "date" && $value[0] == ">") { $db_vars = " AND date_added" . ' >= "'.date("Y-m-d", strtotime($trimChar)).'"' . @$db_vars; }
+  elseif ($key == "date" && $value[0] == "<") { $db_vars = " AND date_added" . ' <= "'.date("Y-m-d", strtotime($trimChar)).'"' . @$db_vars; }
+  elseif ($key == "date") { $db_vars = " AND date_added" . ' = "'.date("Y-m-d", strtotime($value)).'"' . @$db_vars; }
+  elseif ($key == "has_street_view" && $value == "true") $db_vars = " AND street-view_a != '' " . @$db_vars;
+  elseif ($key == "has_street_view" && $value == "false") $db_vars = " AND street-view_a = '' " . @$db_vars;
+  elseif ($value[0] == "!") { $db_vars = " AND NOT " . $key . ' = "'.$trimChar.'"' . @$db_vars; }
+  elseif ($value[0] == ">") { $db_vars = " AND ". $key . ' > '.$trimChar . @$db_vars; }
+  elseif ($value[0] == "<") { $db_vars = " AND ". $key . ' < '.$trimChar . @$db_vars; }
+  elseif ($key == "idlist") { $db_vars = " AND FIND_IN_SET(`id`, '$value')" . @$db_vars; }
+  elseif ($key == "fileSearch") { $db_vars = " AND evidence_a like '%$fs%' OR evidence_b like '%$fs%' OR evidence_c like '%$fs%' OR photo_a like '%$fs%' OR photo_b like '%$fs%' OR photo_c like '%$fs%' OR photo_d like '%$fs%' OR photo_e like '%$fs%' OR photo_f like '%$fs%' OR extra_a like '%$fs%' OR extra_b like '%$fs%' OR extra_c like '%$fs%'" . @$db_vars; }
+  elseif ($key == "address") { $db_vars = " AND " . $key . ' like "%'.$value.'%"' . @$db_vars; }
+  elseif ($key == "tags") { $db_vars = " AND ". $key . ' like "%'.$value.'%"' . @$db_vars; }
+  else { $db_vars = " AND ". $key . ' = "'.$value.'"' . @$db_vars; }
+ }
 }
 if (!empty($db_vars)) $db_vars = "WHERE " . substr(strstr($db_vars," "), 1);
 @$db_vars = str_replace('WHERE AND', 'WHERE', @$db_vars);
