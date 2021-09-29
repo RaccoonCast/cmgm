9a84fc219997841e53d0f656da01c478a44150cd
Spane Boswell <spaneboswell101@gmail.com>
2021-09-29 13:05:52 -0700

    Update LFMF for https://github.com/RaccoonCast/cmgm/commit/0b77041a71b10f4cbf27fe0ed4cc0486bf6051a0

--- cmgm/database/LFMF.php
+++ cmgm/database/LFMF.php
@@ -23,20 +23,20 @@ while($row = $result->fetch_assoc()) {
     foreach ($row as $key => $value) {
         $$key = $value;
 
-        if (!empty($evidence_a) && substr($evidence_a, 0, 4) != "http" && !file_exists("uploads/" . $evidence_a)) echo " $id - EVIDENCE_A<br>";
-        if (!empty($evidence_b) && substr($evidence_b, 0, 4) != "http" && !file_exists("uploads/" . $evidence_b)) echo " $id - EVIDENCE_B<br>";
-        if (!empty($evidence_c) && substr($evidence_c, 0, 4) != "http" && !file_exists("uploads/" . $evidence_c)) echo " $id - EVIDENCE_C<br>";
+        if (!empty($evidence_a) && substr($evidence_a, 0, 4) != "http" && substr($evidence_a, 0, 1) != "#" && !file_exists("uploads/" . $evidence_a)) echo " $id - EVIDENCE_A<br>";
+        if (!empty($evidence_b) && substr($evidence_b, 0, 4) != "http" && substr($evidence_b, 0, 1) != "#" && !file_exists("uploads/" . $evidence_b)) echo " $id - EVIDENCE_B<br>";
+        if (!empty($evidence_c) && substr($evidence_c, 0, 4) != "http" && substr($evidence_c, 0, 1) != "#" && !file_exists("uploads/" . $evidence_c)) echo " $id - EVIDENCE_C<br>";
 
-        if (!empty($photo_a) && substr($photo_a, 0, 4) != "http" && !file_exists("uploads/" . $photo_a)) echo " $id - PHOTO_A<br>";
-        if (!empty($photo_b) && substr($photo_b, 0, 4) != "http" && !file_exists("uploads/" . $photo_b)) echo " $id - PHOTO_B<br>";
-        if (!empty($photo_c) && substr($photo_c, 0, 4) != "http" && !file_exists("uploads/" . $photo_c)) echo " $id - PHOTO_C<br>";
-        if (!empty($photo_d) && substr($photo_d, 0, 4) != "http" && !file_exists("uploads/" . $photo_d)) echo " $id - PHOTO_D<br>";
-        if (!empty($photo_e) && substr($photo_e, 0, 4) != "http" && !file_exists("uploads/" . $photo_e)) echo " $id - PHOTO_E<br>";
-        if (!empty($photo_f) && substr($photo_f, 0, 4) != "http" && !file_exists("uploads/" . $photo_f)) echo " $id - PHOTO_F<br>";
+        if (!empty($photo_a) && substr($photo_a, 0, 4) != "http" && substr($photo_a, 0, 1) != "#" && !file_exists("uploads/" . $photo_a)) echo " $id - PHOTO_A<br>";
+        if (!empty($photo_b) && substr($photo_b, 0, 4) != "http" && substr($photo_b, 0, 1) != "#" && !file_exists("uploads/" . $photo_b)) echo " $id - PHOTO_B<br>";
+        if (!empty($photo_c) && substr($photo_c, 0, 4) != "http" && substr($photo_c, 0, 1) != "#" && !file_exists("uploads/" . $photo_c)) echo " $id - PHOTO_C<br>";
+        if (!empty($photo_d) && substr($photo_d, 0, 4) != "http" && substr($photo_d, 0, 1) != "#" && !file_exists("uploads/" . $photo_d)) echo " $id - PHOTO_D<br>";
+        if (!empty($photo_e) && substr($photo_e, 0, 4) != "http" && substr($photo_e, 0, 1) != "#" && !file_exists("uploads/" . $photo_e)) echo " $id - PHOTO_E<br>";
+        if (!empty($photo_f) && substr($photo_f, 0, 4) != "http" && substr($photo_f, 0, 1) != "#" && !file_exists("uploads/" . $photo_f)) echo " $id - PHOTO_F<br>";
 
-        if (!empty($extra_a) && substr($extra_a, 0, 4) != "http" && !file_exists("uploads/" . $extra_a)) echo " $id - EXTRA_A<br>";
-        if (!empty($extra_b) && substr($extra_b, 0, 4) != "http" && !file_exists("uploads/" . $extra_b)) echo " $id - EXTRA_B<br>";
-        if (!empty($extra_c) && substr($extra_c, 0, 4) != "http" && !file_exists("uploads/" . $extra_c)) echo " $id - EXTRA_C<br>";
+        if (!empty($extra_a) && substr($extra_a, 0, 4) != "http" && substr($extra_a, 0, 1) != "#" && !file_exists("uploads/" . $extra_a)) echo " $id - EXTRA_A<br>";
+        if (!empty($extra_b) && substr($extra_b, 0, 4) != "http" && substr($extra_b, 0, 1) != "#" && !file_exists("uploads/" . $extra_b)) echo " $id - EXTRA_B<br>";
+        if (!empty($extra_c) && substr($extra_c, 0, 4) != "http" && substr($extra_c, 0, 1) != "#" && !file_exists("uploads/" . $extra_c)) echo " $id - EXTRA_C<br>";
         if ($key != "id")$$key = null;
 } }
 $result->close(); $conn->close();
