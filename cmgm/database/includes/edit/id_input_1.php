52606e18ab79c36d0c5c5510066e56e29ec4cac6
Spane Boswell <spaneboswell101@gmail.com>
2021-09-29 20:01:43 -0700

    Improve Search UI for when ID not specified on desktop & mobile.

--- cmgm/database/includes/edit/id_input_1.php
+++ cmgm/database/includes/edit/id_input_1.php
@@ -1,4 +1,4 @@
 <p class="warning">No results found.</p>
 <form action="Edit.php" autocomplete="off" method="get">
-<input type="text" class="id_input" value="" placeholder="Any LTE/NR ID" name="id"><input type="submit" class="small-sb" value="ID Search"><input type="submit" class="small-sb" name="locsearch" value="Location">
+<input type="text" class="id_input" value="" placeholder="" name="id"><input type="submit" class="small-sb" value="ID Search"><input type="submit" class="small-sb" name="locsearch" value="Location Search">
 </form>
