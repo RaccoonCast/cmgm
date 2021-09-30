7fa4f01a20776bfc1e4b0b7b0e903239778aba85
Spane Boswell <spaneboswell101@gmail.com>
2021-09-29 20:09:00 -0700

    Keep NESW in addresses.

--- cmgm/includes/convert/get-address-for-loc.php
+++ cmgm/includes/convert/get-address-for-loc.php
@@ -18,18 +18,6 @@ $response = json_decode($response);
      if ($addrComp->types[0] == 'administrative_area_level_1') $state = $addrComp->short_name;
      }
 
-    // Remove blank spaces
-    if(isset($short_street_name) AND isset($short_street_name)) {
-    $directionCheck_a = explode(' ',trim($short_street_name));
-    $directionCheck_b = explode(' ',trim($long_street_name));
-
-    // Remove - North / East / South / West & N / E / S / W
-    if ("$directionCheck_a[0]" == "N" && "$directionCheck_b[0]" == "North") {$long_street_name = substr($long_street_name,6); }
-    if ("$directionCheck_a[0]" == "E" && "$directionCheck_b[0]" == "East")  {$long_street_name = substr($long_street_name,5); }
-    if ("$directionCheck_a[0]" == "S" && "$directionCheck_b[0]" == "South") {$long_street_name = substr($long_street_name,6); }
-    if ("$directionCheck_a[0]" == "W" && "$directionCheck_b[0]" == "West")  {$long_street_name = substr($long_street_name,5); }
-    }
-
     if(!isset($number)) $number = null;
     if(!isset($long_street_name)) $long_street_name = null;
     $address = "$number $long_street_name";
