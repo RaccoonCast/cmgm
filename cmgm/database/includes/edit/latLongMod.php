32d88c8821cadec55933b912f5cf4fd766366c14
Spane Boswell <spaneboswell101@gmail.com>
2021-10-23 16:33:40 -0700

    Tweak default seperation distance of RML on latLongMod.php & add customizable multiplier.

--- cmgm/database/includes/edit/latLongMod.php
+++ cmgm/database/includes/edit/latLongMod.php
@@ -1,9 +1,14 @@
 <?php
 $multiplier = 1;
-$modDigit = 0.000075;
+if (isset($_GET['multiplier'])) $multiplier = $_GET['multiplier'];
+$modDigit = 0.00006;
 $modDigit = $modDigit * $multiplier;
 ?>
 <?php if($isMobile == "false") {?>
+<?php if (!isset($delete) && !isset($_GET['new'])) {?>
+<a style="color: blue;" href="Edit.php?id=<?php echo $id; ?>&multiplier=<?php echo $multiplier - 0.1; ?>">-</a>
+<a style="color: blue;" href="Edit.php?id=<?php echo $id; ?>&multiplier=<?php echo $multiplier + 0.1; ?>">+</a>
+<?php } ?>
 <a href="javascript:;" onclick="copyToClipboard('<?php echo $latitude + $modDigit . "," . $longitude - $modDigit ?>')" >R</a>
 <a href="javascript:;" onclick="copyToClipboard('<?php echo $latitude + $modDigit . "," . $longitude ?>')">M</a>
 <a href="javascript:;" onclick="copyToClipboard('<?php echo $latitude + $modDigit . "," . $longitude + $modDigit ?>')" >L</a>
