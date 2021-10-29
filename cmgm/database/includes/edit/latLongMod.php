0bc504391526b374b5bb8df8e7b1623ec797c902
Spane Boswell <spaneboswell101@gmail.com>
2021-10-28 18:08:01 -0700

    whoops

--- cmgm/database/includes/edit/latLongMod.php
+++ cmgm/database/includes/edit/latLongMod.php
@@ -9,8 +9,8 @@ $modDigit = $modDigit * $multiplier;
 <a style="color: blue;" href="Edit.php?id=<?php echo $id; ?>&multiplier=<?php echo $multiplier - 0.1; ?>">-</a>
 <a style="color: blue;" href="Edit.php?id=<?php echo $id; ?>&multiplier=<?php echo $multiplier + 0.1; ?>">+</a>
 <?php } ?>
-<a href="javascript:;" onclick="copyToClipboard('<?php echo $latitude + $modDigit . "," . $longitude - $modDigit ?>')" >R</a>
+<a href="javascript:;" onclick="copyToClipboard('<?php echo $latitude + $modDigit . "," . $longitude - $modDigit ?>')" >L</a>
 <a href="javascript:;" onclick="copyToClipboard('<?php echo $latitude + $modDigit . "," . $longitude ?>')">M</a>
-<a href="javascript:;" onclick="copyToClipboard('<?php echo $latitude + $modDigit . "," . $longitude + $modDigit ?>')" >L</a>
+<a href="javascript:;" onclick="copyToClipboard('<?php echo $latitude + $modDigit . "," . $longitude + $modDigit ?>')" >R</a>
 <?php } ?>
 <a href="javascript:;" class="pad-small-link" onclick="copyToClipboard('<?php echo $latitude . "," . $longitude; ?>')">Copy</a>
