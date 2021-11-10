5c38766fdf8f86fb0b450e2434a358aa8b4d0854
Spane Boswell <spaneboswell101@gmail.com>
2021-11-09 22:25:53 -0800

    Some spaghetti code to make the-edit-code split up comma-separated lat,long

--- cmgm/database/includes/edit/the_edit_code.php
+++ cmgm/database/includes/edit/the_edit_code.php
@@ -3,7 +3,20 @@
 $sql_edit = "UPDATE database_db SET ";
 $vals = "$";
 if (isset($_POST['edittag'])) foreach ($_POST as $key => $value) {
-  if (@${@$key} != $value && $key != "evidence_score" && $key != "edittag" && $key != "edit_history" && @$key != "edit_lock" && @$key != "id" && @$key != "new" && @$key != "date_added") {
+  if ($key == "latitude") {
+    if(strpos($value, ',') !== false) {
+    $result = explode(",", $value);
+    $tmp_latitude = trim($result[0]);
+    $tmp_longitude = trim($result[1]);
+
+    if ((is_numeric($tmp_latitude)) and (is_numeric($tmp_longitude))) {
+      $sql_edit .= "latitude = '".mysqli_real_escape_string($conn, $tmp_latitude)."', ";
+      $sql_edit .= "longitude = '".mysqli_real_escape_string($conn, $tmp_longitude)."', ";
+    }
+  } else {
+    $sql_edit .= "$key = '".mysqli_real_escape_string($conn, $value)."', ";
+  }
+  } elseif (@${@$key} != $value && $key != "evidence_score" && $key != "edittag" && $key != "edit_history" && @$key != "edit_lock" && @$key != "id" && @$key != "new" && @$key != "date_added") {
    if (strpos($key, 'sv') === false) $sql_edit .= "$key = '".mysqli_real_escape_string($conn, $value)."', ";
    if (strpos($key, 'sv') !== false) $sql_edit .= "$key = '".mysqli_real_escape_string($conn, str_replace("https://", "",$value))."', ";
    @$vals .= $key . ", $";
@@ -20,6 +33,7 @@ if (strlen($sql_edit) != 23) {
   if (isset($edit_history)) $edit_history .= "\r\n";
   $sql_edit .= " edit_history = '$edit_history" . date("Y-m-d H:i:s") . " PST: $username ($userIP) edited $vals.' WHERE id = $id";
   if (is_numeric($_POST['latitude']) && is_numeric($_POST['longitude'])) mysqli_query($conn, $sql_edit);
+  if (is_numeric(@$tmp_latitude) && is_numeric(@$tmp_longitude)) mysqli_query($conn, $sql_edit);
 
   include "read_data.php";
 }
