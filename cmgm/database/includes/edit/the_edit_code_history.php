e5e1df5c44855c8ed1eb4f32dad43c4abb579aab
Spane Boswell <spaneboswell101@gmail.com>
2021-11-14 14:59:56 -0800

    Forgot something.

--- cmgm/database/includes/edit/the_edit_code_history.php
+++ cmgm/database/includes/edit/the_edit_code_history.php
@@ -1,7 +1,7 @@
 <?php
 if (!isset($_POST['new'])) {
   if (!empty($value)) {
-    if (!empty(@${@$key})) @$vals .= "$" . $key . ' value changed from "' . @${@$key} . '" to "' . preg_replace( "/\r|\n/", "", $value ) . '" \r\n';
+    if (!empty(@${@$key})) @$vals .= "$" . $key . ' value changed from "' . preg_replace( "/\r|\n/", "", @${@$key}) . '" to "' . preg_replace( "/\r|\n/", "", $value ) . '" \r\n';
     if (empty(@${@$key})) @$vals .= "$" . $key . ' value set to "' . preg_replace( "/\r|\n/", "", $value ) . '" \r\n';
   }
   if (empty($value)) @$vals .= "$" . $key . ' value removed, $' . $key . ' formerly "' . preg_replace( "/\r|\n/", "", @${@$key}) . '" \r\n';
