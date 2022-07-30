33f34ca12ae2cd0b24999e3f51159221262d6ea8
Spane Boswell <spaneboswell101@gmail.com>
2022-07-30 03:24:51 -0700

    maybe fix some warnings

--- cmgm/functions.php
+++ cmgm/functions.php
@@ -2,6 +2,7 @@
 include 'includes/functions/getGetVars.php';
 include 'includes/functions/basic-functions.php';
 include 'includes/useridsys/native.php';
+include SITE_ROOT . "/includes/functions/sqlpw.php";
 include 'includes/functions/css.php';
 include 'includes/functions/headhtml.php';
 include 'includes/functions/common-debug-info.php';
