61e3c3960bdee3239c6c5d1a7c58d23151a5eee5
Spane Boswell <spaneboswell101@gmail.com>
2022-07-30 07:09:22 -0700

    Revamp Search UI phase 1/3

--- cmgm/database/DB.php
+++ cmgm/database/DB.php
@@ -1,44 +1,55 @@
 <!doctype html>
 <html lang="en">
 <head>
+  <script src="../js/redir.js"></script>
   <?php
   include '../functions.php';
   if (!isset($_GET['latitude'])) $latitude = $default_latitude;
   if (!isset($_GET['longitude'])) $longitude = $default_longitude;
-  include "includes/DB-filter.php";
+  include "includes/DB-filter-post.php";
   include "../includes/link-conversion-and-handling/function_goto.php";
   ?>
 </head>
 <body>
 <?php
+if (!isset($limit)) $limit = "100";
 $sql = "SELECT DISTINCT *, (3959 * ACOS(COS(RADIANS($latitude)) * COS(RADIANS(latitude)) * COS(RADIANS(longitude) - RADIANS($longitude)) + SIN(RADIANS($latitude)) * SIN(RADIANS(latitude)))) AS DISTANCE FROM db ".@$db_vars." ORDER BY distance LIMIT $limit";
 if (isset($_GET['showsql'])) echo $sql;
 $result = mysqli_query($conn,$sql);
-$counter=0;
-while($row = $result->fetch_assoc()) {
+if (mysqli_num_rows($result) == "1") {
+  while($row = $result->fetch_assoc()) {
+  foreach ($row as $key => $value) {
+    $$key = $value;
+    if ($key == "DISTANCE") { redir("Edit.php?id=$id","0"); }
+  }
+}
+}
+if (mysqli_num_rows($result) == "0") {
+  echo "<br> No results found.";
+  redir("Search.php?latitude=$latitude&longitude=$longitude","1");
+} ?>
+<table border="1">
+<thead>
+<tr>
+  <th class="btn-holder-edit">Edit</th>
+  <th class="enb">eNB</th>
+  <th class="carrier">Carrier</th>
+  <th class="address">Address</th>
+  <th class="notes">Notes</th>
+  <th class="btn-holder-evidence">Evidence</th>
+</tr>
+</thead>
+<tbody> <?php
+if (mysqli_num_rows($result) > 1) { while($row = $result->fetch_assoc()) {
     foreach ($row as $key => $value) {
-      $counter++;
-      if ($counter==1) { ?>
-        <table border="1">
-        <thead>
-        <tr>
-          <th>LTE #</th>
-          <?php if($isMobile != "true") {?><th>Carrier</th> <?php } ?>
-          <th>Address</th>
-          <?php if($isMobile != "true") {?><th>Widgets</th> <?php } ?>
-          <th>Notes</th>
-          <th>EV</th>
-          <?php if($isMobile != "true") {?><th>Score</th> <?php } ?>
-        </tr>
-        </thead>
-        <tbody> <?php
-      }
       $$key = $value;
       if ($key == "DISTANCE") {
         echo "<tr>";
-        $db_map_link = "https://cmgm.ml/database/Map.php?latitude=".$latitude."&longitude=".$longitude."&zoom=18&back=DB";
-        $gmlink = function_goto($latitude,$longitude,NULL,NULL,NULL,NULL,NULL,"Google Maps",NULL,$cm_mapType,$cm_groupTowers,$cm_showLabels,$cm_showLowAcc,$cm_zoom,@$cm_netType);
+        $db_map_link = "https://cmgm.us/database/Map.php?latitude=".$latitude."&longitude=".$longitude."&zoom=18&back=DB";
+        $gmlink = function_goto($latitude,$longitude,NULL,NULL,NULL,NULL,NULL,"Maps",NULL,$cm_mapType,$cm_groupTowers,$cm_showLabels,$cm_showLowAcc,$cm_zoom,@$cm_netType);
         $cmlink = function_goto($latitude,$longitude,$carrier,NULL,NULL,NULL,NULL,"CellMapper",NULL,$cm_mapType,$cm_groupTowers,$cm_showLabels,$cm_showLowAcc,$cm_zoom,@$cm_netType);
+
+          ?> <td><input type="button" class="w-100 btn-edit" onclick="redir('Edit.php?id=<?php echo $id;?>','0')"value="Edit"></input></td> <?php
 
         if($isMobile == "true") {
         echo '<td class="lte" id="'.$id.'">'.$carrier.'<br><a href="'.$cmlink.'">'.$LTE_1.'</a></td>';
@@ -55,11 +66,6 @@ while($row = $result->fetch_assoc()) {
 
           if($isMobile == "true") if (!empty($notes)) echo nl2br("<td class="."notes"."><div class="."notes-text".">".$notes."</div>");
           if($isMobile == "true") if (empty($notes)) echo nl2br("<td class="."notes"."><div class="."notes-text"."></div>");
-          if($isMobile != "true") echo nl2br("<td class="."widget-td"." style="."text-align: center;".">");
-
-          ?><div class="widget-box"><?php
-          include SITE_ROOT."/includes/widgets.php";
-          ?></td></div><?php
 
           if($isMobile != "true") {
             if (!empty($notes)) echo nl2br("<td class="."notes".">".$notes."</td>");
@@ -68,36 +74,25 @@ while($row = $result->fetch_assoc()) {
 
         if(substr($evidence_a, 0, 6) == "image-") $evidence_a = "uploads/$evidence_a";
 
-        if (!empty($evidence_a)) {
-          if(substr($evidence_a, 0, 6) == "image-") {
-            if (file_exists($evidence_a)) {
-            if($isMobile == "true") echo "<td class="."ev"."><a target="."_blank"." href="."$evidence_a".">Evidence</a>";
-            if($isMobile != "true") echo "<td class="."ev"."><a target="."_blank"." href="."$evidence_a".">Evidence</a></td>";
-          } else {
-            if($isMobile == "true") echo "<td class="."ev".">Evidence is missing";
-            if($isMobile != "true") echo "<td class="."ev".">Evidence is missing</td>";
-          }
-        } else {
-          if($isMobile == "true") echo "<td class="."ev"."><a target="."_blank"." href="."$evidence_a".">Evidence</a>";
-          if($isMobile != "true") echo "<td class="."ev"."><a target="."_blank"." href="."$evidence_a".">Evidence</a></td>";
-        }
-        } else {
-        if($isMobile != "true") echo "<td class="."ev"."></td>";
-        }
-        include '../includes/functions/calculateEV-math.php';
-        if($isMobile != "true") echo "<td>".$ev."</td>";
+        if ((substr($evidence_a, 0, 6) != "image-") OR (file_exists($evidence_a))) { ?>
+          <td><div class="btn-group"><input type="button" class="btn-evidence" value="Copy evidence"></input> <input
+         type="button" class="btn-evidence"onclick="redir('<?php echo $evidence_a; ?>','0')" value="View evidence"></input></td></div> <?php } else {
+           echo "Missing file!";
+         }
+
+
+
         echo "</tr>"."\n";
+
       }
-      }
-      }
-      if($counter==0){
-      ?> <br> <?php
-      echo " No results found.";
-      redir("Search.php?latitude=$latitude&longitude=$longitude","1");
-    } else {}
+    }}}
 ?>
 </tbody>
 </table>
-<?php include "includes/footer.php"; ?>
+<?php
+if (mysqli_num_rows($result) > 40) {
+  echo "<br>";
+}
+include "includes/footer.php"; ?>
 </body>
 </html>
--- cmgm/database/Search.php
+++ cmgm/database/Search.php
@@ -11,11 +11,28 @@
    <body>
       <?php
          ?>
-      <form autocomplete="off"  method="get">
+      <form autocomplete="off"  method="post">
         <div class="buttons">
-          <h3>Search</h3><label class="label">Cell Site Type</label><select
-        class="custominput cellsite-type-custom-width dropdown" name="cellsite_type">
-        <option selected="selected" value="">All</option>
+          <h3>Search</h3><!-- <label class="label">eNB / gNB</label>--><input
+          class="custominput" placeholder="eNB / gNB" type="text" maxlength="30" name="id"><br><!--<label class="label">Cell Site Type</label>--><select
+          class="w-20 custominput cellsite-type-custom-width dropdown" name="status">
+          <option selected="selected" value="">Status: All</option>
+          <option value="verified" >Verified</option>
+          <option value="unverified">Unverified</option>
+        </select><select class="w-32 dropdown custominput" autocomplete="on" name="concealed">
+            <option selected="selected" value="">Conlcealment: n/a</option>
+            <option value="true">Concealed</option>
+            <option value="false">Unconcealed</option>
+            </select><select
+               class="custominput w-23 dropdown" autocomplete="on" name="carrier">
+              <option selected="selected" value="">Carrier: Any</option>
+              <option value="T-Mobile">T-Mobile</option>
+              <option value="ATT">AT&T</option>
+              <option value="Verizon">Verizon</option>
+              <option value="Sprint">Sprint</option>
+           </select><select
+        class="custominput cellsite-type-custom-width dropdown w-25" name="cellsite_type">
+        <option selected="selected" value="">Tower type: All</option>
         <option value="tower">Tower</option>
         <option value="rooftop">Rooftop</option>
         <option value="tank">Tank</option>
@@ -27,53 +44,37 @@
         <option value="pole">Pole</option>
         <option value="structure">Disguised structure</option>
         <option value="other">Other/Uknown</option>
-        </select><br><label class="label">Concealment</label><select class="custominput" autocomplete="on" name="concealed">
-        <option selected="selected" value="">Unconcealed/Concealed</option>
-        <option value="true">Concealed</option>
-        <option value="false">Unconcealed</option>
-        </select><br><label class="label">Status</label><select
-        class="custominput cellsite-type-custom-width dropdown" name="status">
-        <option selected="selected" value="">All</option>
-        <option value="verified" selected="selected">Verified</option>
-        <option value="unverified">Unverified</option>
-        </select>
-        </select>
-		       <br><label class="label">Carrier</label><select
-              class="custominput dropdown" autocomplete="on" name="carrier">
-             <option <?php if(empty($carrier)) echo 'selected="selected"';?> value="">All</option>
-             <option <?php if($carrier == "T-Mobile") echo 'selected="selected"';?> value="T-Mobile">T-Mobile</option>
-             <option <?php if($carrier == "ATT") echo 'selected="selected"';?> value="ATT">AT&T</option>
-             <option <?php if($carrier == "Verizon") echo 'selected="selected"';?> value="Verizon">Verizon</option>
-             <option <?php if($carrier == "Sprint") echo 'selected="selected"';?> value="Sprint">Sprint</option>
-          </select>
-		 <br><label class="label">LTE/NR id</label><input
-     class="custominput" type="text" maxlength="30" name="id"><br><label class="label">Creation Date</label><input
+      </select><!--<label class="label">Concealment</label> --><!-- <br>label class="label">Status</label>-->
+      </select>
+		       <!-- <label class="label">Carrier</label>-->
+		 <br><!-- <br><label class="label">Creation Date</label><input
      class="custominput" type="text" maxlength="11" placeholder="1/20/2009" name="date">
-		 <br><label class="label">File</label><input type="text" class="custominput small-text" maxlength="55" name="fileSearch">
-		 <br><label class="label">Address (of site)</label><input type="text" class="custominput" maxlength="50" placeholder="Address" autocomplete="chrome-off" name="address"><input
+		 <br> --><!--<label class="label">File</label>-->
+		 <!--<br><label class="label">Address (of site)</label>--><input type="text" class="custominput" maxlength="50" placeholder="Address" autocomplete="chrome-off" name="address"><input
      class="custominput" type="text" maxlength="25" placeholder="City" autocomplete="chrome-off" name="city"><input
      class="custominput" type="text" maxlength="5" placeholder="Zip" autocomplete="chrome-off" name="zip"><input
      class="custominput" type="text" maxlength="20" placeholder="State" autocomplete="chrome-off" name="state">
-		 <br><label class="label">Near lat/long</label><input type="text" class="latlong custominput" maxlength="45" value="<?php echo $latitude;?>" name="latitude"><input
-     type="text" class="latlong custominput" maxlength="45" value="<?php echo $longitude?>" name="longitude">
-     <br><label class="label">Tag Search</label><input type="text" class="custominput" maxlength="45" name="tags">
-     <br><label class="label">Limits results to</label><input type="text" class="custominput" maxlength="45" value="500" name="limit">
-     <label class="label height38">Has Street View:</label><div class="wrapper-73">
+		 <!--<br><label class="label">Sort results near</label><input type="text" class="latlong custominput" maxlength="45" value="<?php echo $latitude;?>" name="latitude"><input
+     type="text" class="latlong custominput" maxlength="45" value="<?php echo $longitude?>" name="longitude">-->
+     <!--<br><label class="label">Tag filter</label>--><input type="text" class="custominput" maxlength="45" placeholder="Tags filter" name="tags"><input
+     type="text" class="custominput small-text" maxlength="55" placeholder="File search filter" name="fileSearch">
+     <!-- <br><label class="label">Limits results to</label><input type="text" class="custominput" maxlength="45" value="500" name="limit"> -->
+     <!--<label class="label height38">Has Street View:</label><div class="wrapper-73">
      <input type="radio" id="sv_1" name="has_street_view" value="true"><label for="sv_1">true</label>
      <input type="radio" id="sv_2" name="has_street_view" value="false"><label for="sv_2">false</label>
-     </div>
-     <br><label class="label height38">Map Pin Styles</label><div class="wrapper-73">
-       <input type="radio" id="pin_1" name="pin_style" value="default" checked><label for="pin_1">default</label>
-       <input type="radio" id="pin_2" name="pin_style" value="basic"><label for="pin_2">basic</label>
-       <input type="radio" id="pin_3" name="pin_style" value="celltype"><label for="pin_3">cellsite type</label>
-       <input type="radio" id="pin_4" name="pin_style" value="carrier"><label for="pin_4">carrier</label>
-     </div>
+   </div> -->
+     <!-- <br><label class="label height38">Map Pin Styles</label><div class="wrapper-73">
+       <input type="radio" id="pin_1" name="pin_style" value="default" checked><label for="pin_1"> Default</label>
+       <input type="radio" id="pin_2" name="pin_style" value="basic"><label for="pin_2"> Basic</label>
+       <input type="radio" id="pin_3" name="pin_style" value="celltype"><label for="pin_3"> Tower Type</label>
+       <input type="radio" id="pin_4" name="pin_style" value="carrier"><label for="pin_4"> Carrier</label>
+     </div> -->
      <p>
        > = more than/after<br>
        < = less than/before
      </p>
-     <label></label><input type="submit" formaction="Map.php" class="sb custominput" value="Map View"><input
-     type="submit" formaction="DB.php" class="sb custominput" value="List View">
+     <input type="submit" formaction="DB.php" class="sb w-75 custominput cmgm-btn" value="Search"><input
+     type="submit" formaction="Map.php" class="w-25 sb custominput cmgm-btn" value="Search on map">
    </div>
     </form>
     <?php
