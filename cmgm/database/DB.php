a9db0e20cf143cd2be744d5d13acf623b4d05f7a
Spane Boswell <spaneboswell101@gmail.com>
2022-08-27 03:54:56 -0700

    Segregate DB code more

--- cmgm/database/DB.php
+++ cmgm/database/DB.php
@@ -9,22 +9,5 @@
   if (!isset($_GET['longitude'])) $longitude = $default_longitude;
   include "includes/DB-filter-post.php";
   include "../includes/link-conversion-and-handling/function_goto.php";
-
-  if (!isset($limit)) $limit = "100";
-  $sql = "SELECT DISTINCT *, (3959 * ACOS(COS(RADIANS($latitude)) * COS(RADIANS(latitude)) * COS(RADIANS(longitude) - RADIANS($longitude)) + SIN(RADIANS($latitude)) * SIN(RADIANS(latitude)))) AS DISTANCE FROM db ".@$db_vars." ORDER BY distance LIMIT $limit";
-  if (isset($_GET['showsql'])) echo $sql;
-  $result = mysqli_query($conn,$sql);
-  if (mysqli_num_rows($result) == "1") {
-    while($row = $result->fetch_assoc()) {
-    foreach ($row as $key => $value) {
-      $$key = $value;
-      if ($key == "DISTANCE") { redir("Edit.php?id=$id","0"); }
-    }
-  }
-  }
-  if (mysqli_num_rows($result) == "0") {
-    echo "<br> No results found.";
-    redir("Search.php?latitude=$latitude&longitude=$longitude","1");
-  }
-  ?>
+?>
 </head><?php include "includes/DB.php" ?>
--- cmgm/database/includes/DB.php
+++ cmgm/database/includes/DB.php
@@ -1,3 +1,20 @@
+<?php
+$sql = "SELECT DISTINCT id,LTE_1,carrier,latitude,longitude,address,city,state,zip,notes,evidence_a, (3959 * ACOS(COS(RADIANS($latitude)) * COS(RADIANS(latitude)) * COS(RADIANS(longitude) - RADIANS($longitude)) + SIN(RADIANS($latitude)) * SIN(RADIANS(latitude)))) AS DISTANCE FROM db ".@$db_vars." HAVING distance < 0.055 ORDER BY distance LIMIT 75";
+$result = mysqli_query($conn,$sql);
+
+if (mysqli_num_rows($result) == "1") {
+  while($row = $result->fetch_assoc()) {
+  foreach ($row as $key => $value) {
+    $$key = $value;
+    if ($key == "DISTANCE") { redir("Edit.php?id=$id","0"); }
+  }
+}
+}
+if  ((mysqli_num_rows($result) == "0")) {
+  echo "<br> No results found.";
+  redir("Search.php?latitude=$latitude&longitude=$longitude","1");
+}
+?>
 <table border="1">
 <thead>
 <tr>
