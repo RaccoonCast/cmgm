7873031ca387d0f36899ce2166c88f28a4d8308c
Spane Boswell <spaneboswell101@gmail.com>
2022-08-27 02:19:08 -0700

    Fix me-breaking-it

--- cmgm/database/DB.php
+++ cmgm/database/DB.php
@@ -17,24 +17,16 @@ include "../includes/link-conversion-and-handling/function_goto.php";
 //if (!isset($limit)) $limit = "100";
 //echo $sql;
 
-if (isset($_GET['new'])) {
+  ?> <title>CMGM - Edit</title> <?php
   if (@$back_num < 3 & isset($back_num)) redir("Edit.php?id=" . --$id . "&back=" . ++$back_num . "&nolocsearch","0"); // back
   if (@$next_num < 3 & isset($next_num)) redir("Edit.php?id=" . ++$id . "&next=" . ++$next_num . "&nolocsearch","0"); // next
   $id_trim =preg_replace('/\s+/', '', $id);
   if (!empty($id_trim) && is_numeric($id_trim)) $new_id = @mysqli_fetch_array(mysqli_query($conn, "SELECT id FROM db WHERE LTE_1='$id_trim' OR LTE_2='$id_trim' OR LTE_3='$id_trim' OR LTE_4='$id_trim' OR LTE_5='$id_trim' OR LTE_5='$id_trim' OR LTE_6='$id_trim' OR NR_1='$id_trim' OR NR_2='$id_trim'"))['id']; // id search
   if (!empty($new_id)) redir("Edit.php?id=$new_id","0"); // redir to searched ID
-  ?> <title>CMGM - Edit</title> <?php
-  // LAST DITCH EFFORT TO FIGURE OUT WHO WE EDITING
   include SITE_ROOT . "/includes/link-conversion-and-handling/convert.php";
-  }
-//include "includes/edit/id_input/search_page.php";
   [$latitude,$longitude] = convert($q,"HomeSmart",$default_latitude,$default_longitude,$maps_api_key,$userID,$default_carrier,$cm_mapType,$cm_groupTowers,$cm_showLabels,$cm_showLowAcc,$cm_zoom);
   $sql = "SELECT DISTINCT *, (3959 * ACOS(COS(RADIANS($latitude)) * COS(RADIANS(latitude)) * COS(RADIANS(longitude) - RADIANS($longitude)) + SIN(RADIANS($latitude)) * SIN(RADIANS(latitude)))) AS DISTANCE FROM db ".@$db_vars." HAVING distance < 0.055 ORDER BY distance LIMIT $limit";
   $result = mysqli_query($conn,$sql);
-}
-
-
-
 
 if (mysqli_num_rows($result) == "1") {
   while($row = $result->fetch_assoc()) {
