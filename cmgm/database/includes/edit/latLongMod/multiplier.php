0f37736fe9b885b8269f09243727dc2545e9b82c
Spane Boswell <spaneboswell101@gmail.com>
2022-08-27 01:25:04 -0700

    delete file that shouldve been deleted a long ago

--- cmgm/database/includes/edit/latLongMod/multiplier.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Multiplier
-$multiplier = 1;
-if (isset($_GET['multiplier'])) $multiplier = $_GET['multiplier'];
-$modDigit = 0.00006;
-$modDigit = $modDigit * $multiplier;
-?>
-<a href="Edit.php?id=<?php echo $id; ?>&multiplier=<?php echo $multiplier - 0.1; ?>">-</a>
-<a class="pad-small-link-right" href="Edit.php?id=<?php echo $id; ?>&multiplier=<?php echo $multiplier + 0.1; ?>">+</a>
