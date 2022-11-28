52fdf7e169d51e4a8a8da686822433d0d90bc4de
Spane Boswell <spaneboswell101@gmail.com>
2022-11-28 11:11:52 -0800

    Revert "Revert "Solve world hunger by removing bad code""
    This reverts commit c364f5c90211b3cdfce40e3a8beb8d485dcf7db3.

--- cmgm/api/pciPlusSearch.php
+++ cmgm/api/pciPlusSearch.php
@@ -2,7 +2,7 @@
 //  cody and alps' purple iphones (CAAPI)
 header("Access-Control-Allow-Origin: *");
 header('Content-type: application/json');
-header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
+
 header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Cache-Control: post-check=0, pre-check=0', FALSE);
 header('Pragma: no-cache');
