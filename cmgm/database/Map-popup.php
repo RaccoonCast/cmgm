b03ad88791f8652b9d05ec3c9f2cedd979a01b78
Spane Boswell <spaneboswell101@gmail.com>
2022-12-12 14:24:52 -0800

    Redesigned map-popup, still a wip.

--- cmgm/database/Map-popup.php
+++ cmgm/database/Map-popup.php
@@ -5,93 +5,77 @@
   $allowGuests = "true";
   include '../functions.php';
   include "../includes/functions/calculateEV.php";
+  $id = $_GET['mp-id'];
   ?>
   <script src="../js/redirAtPos.js"></script>
+  <script>
+         function openEditLink() {  window.open("<?php echo $domain_with_http; ?>/database/Edit.php?id=<?php echo $id; ?>", "_blank"); }
+         function openDeleteLink() {  window.open("<?php echo $domain_with_http; ?>/database/Edit.php?id=<?php echo $id; ?>&delete=false&redirPage=Map-popup", "_self"); }
+     </script>
 </head>
 <body class="body">
 <?php
-$id = $_GET['mp-id'];
 
-$database_get_list = "id,date_added,LTE_1,LTE_2,LTE_3,LTE_4,LTE_5,LTE_6,carrier,latitude,longitude,city,zip,state,address,notes,evidence_score,evidence_a,sv_a";
+
+$database_get_list = "id,date_added,LTE_1,LTE_2,LTE_3,LTE_4,LTE_5,LTE_6,carrier,latitude,longitude,city,zip,state,address,notes,evidence_score,evidence_a,sv_a,sv_b,sv_c,sv_d,sv_e,sv_f,cellsite_type,concealed";
 
 $sql = "SELECT $database_get_list FROM db WHERE id = $id;";
 $result = mysqli_query($conn, $sql);
 
-while ($row = mysqli_fetch_assoc($result)) { // Important line !!! Check summary get row on array ..
-  $colCount = 1;
+$sql_read_result = mysqli_query($conn,$sql);
+while($row = $sql_read_result->fetch_assoc()) foreach ($row as $key => $value) $$key = $value;
 
-    foreach ($row as $field => $value) {
+                    if(!empty($LTE_1)) { $lte_list = $LTE_1; }
+                    if(!empty($LTE_2)) { $lte_list = $lte_list . ", " . $LTE_2; }
+                    if(!empty($LTE_3)) { $lte_list = $lte_list . ", " . $LTE_3; }
+                    if(!empty($LTE_4)) { $lte_list = $lte_list . ", " . $LTE_4; }
+                    if(!empty($LTE_5)) { $lte_list = $lte_list . ", " . $LTE_5; }
+                    if(!empty($LTE_6)) { $lte_list = $lte_list . ", " . $LTE_6; }
+                    if(!empty($NR_1)) { $ltenr_list = ", " . $ltenr_list . ", " . $NR_1; }
+                    if(!empty($NR_2)) { $ltenr_list = ", " . $ltenr_list . ", " . $NR_2; }
 
-      $sepCount = ($colCount++);
+                    if(!empty($sv_a)) { $sv_list = '<a href="https://'.$sv_a.'">SV_A</a>'; }
+                    if(!empty($sv_b)) { $sv_list = $sv_list . " | " . '<a target="_blank" href="https://'.$sv_b.'">SV_B</a>'; }
+                    if(!empty($sv_c)) { $sv_list = $sv_list . " | " . '<a target="_blank" href="https://'.$sv_c.'">SV_C</a>'; }
+                    if(!empty($sv_d)) if (isMobile()) $sv_list = $sv_list . '<br><a target="_blank" href="https://'.$sv_d.'">SV_D</a>';
+                    if(!empty($sv_d)) if (!isMobile()) { $sv_list = $sv_list . " | " . '<a target="_blank" href="https://'.$sv_d.'">SV_D</a>'; }
+                    if(!empty($sv_e)) { $sv_list = $sv_list . " | " . '<a target="_blank" href="https://'.$sv_e.'">SV_E</a>'; }
+                    if(!empty($sv_f)) { $sv_list = $sv_list . " | " . '<a target="_blank" href="https://'.$sv_f.'">SV_F</a>'; }
+                    if (empty($sv_a)) $sv_list = "Street View";
 
-                  switch ($sepCount) {
-                    case 1:  $id = $value; break;
-                    case 2:  $date_added = $value; break;
-                    case 3:  $LTE_1 = $value; break;
-                    case 4:  $LTE_2 = $value; break;
-                    case 5:  $LTE_3 = $value; break;
-                    case 6:  $LTE_4 = $value; break;
-                    case 7:  $LTE_5 = $value; break;
-                    case 8:  $LTE_6 = $value; break;
-                    case 9:  $carrier = $value; break;
-                    case 10:  $latitude = $value; break;
-                    case 11:  $longitude = $value; break;
-                    case 12:  $city = $value; break;
-                    case 13:  $zip = $value; break;
-                    case 14:  $state = $value; break;
-                    case 15:  $address = $value; break;
-                    case 16:  $notes = $value; break;
-                    case 17:  $evidence_score = $value; break;
-                    case 18:  $evidence_a = $value; break;
-                    case 19:  $sv_a = $value;
+                    $concealed_status = ($concealed == "true") ? "Concealed" : "Unconcealed";
+                    ?>
 
-                    echo nl2br("<p class=" . "widget" . ">" . $carrier . " <a href=" . "Reader.php?back_url=Map-popup&mp-id=" . $id . ">#" . $id . "</a> </p>");
-                    $no_reader = "true";
-                    $no_map = "true";
-                    $redirPage = "Map-popup";
-                    include "../includes/widgets.php";
-                    ?> <br> <?php
+                    <table>
+                    <thead>
+                    <tr id="tr-even">
+                    <td colspan="2"><?php echo $concealed_status . " " . $carrier  . " " .  ucfirst($cellsite_type) . " #" . $id ?></td>
+                    </tr>
+                    </thead>
 
-                    if (empty($LTE_1)) $LTE_1 = "CellMapper";
-                      include "$SITE_ROOT/includes/misc-functions/cm_linkgen.php";
-                      $cellmapper_net_url = cellmapperLink($latitude,$longitude,$cm_zoom,$carrier,"LTE",$cm_mapType,$cm_groupTowers,$cm_showLabels,$cm_showLowAcc);
-                        if(!empty($LTE_1)) ?> <a class="widget" target="_blank" href="<?php echo $cellmapper_net_url ?>"><?php echo $LTE_1?><?php
-                        if(!empty($LTE_2)) { echo " & " . $LTE_2; }
-                        if(!empty($LTE_3)) { echo " & " . $LTE_3; }
-                        if(!empty($LTE_4)) { echo " & " . $LTE_4; }
-                        if(!empty($LTE_5)) { echo " & " . $LTE_5; }
-                        if(!empty($LTE_6)) { echo " & " . $LTE_6; }
-                        if(!empty($NR_1)) { echo " & " . $NR_1; }
-                        if(!empty($NR_2)) { echo " & " . $NR_2; } ?> </a> <?php
-                        echo "<br>";
+                    <tr class="tr-odd">
+                    <td>eNBs</td>
+                    <td><?php echo $lte_list ?></td>
+                    </tr>
 
-                      echo nl2br('<a target="_blank" href="https://maps.google.com/maps?f=q&source=s_q&hl=en&q=' .$latitude . ',' .$longitude . '">' . $address . ' <br>' . $city . ', ' . $state . ' ' . $zip . '</a>');
-                      $recalcEV = calculateEV($id,$carrier);
-                      if (!empty($recalcEV)) {
-                      echo "<p>Evidence Score: " . $recalcEV . "";
-                      }
+                    <tr class="tr-even">
+                    <td>Actions</td>
+                    <td><input onclick="openDeleteLink()" type="button" value="Delete"><input type="button" value="View Notes"><input onclick="openEditLink()" type="button" value="Edit"></td>
+                    </tr>
 
-                      if (!empty($evidence_score)) {
-                      echo "( Old: " . $evidence_score . " )</p>";
-                      } else {
-                      echo "</p>";
-                      }
+                    <tr class="tr-odd">
+                    <td rowspan="2">Address</td>
+                    <td><?php echo $address ?></td>
+                    </tr>
 
-                      if(substr($evidence_a, 0, 6) == "image-") {
-                         $evidence_a = "uploads/$evidence_a";
-                      }
+                    <tr class="tr-odd">
+                    <td><?php echo $city . " " . $state ?>, <?php echo $zip ?></td>
+                    </tr>
 
-                      if (!empty($evidence_a)) {
-                        echo ("<a target=" . "_blank" . " href=" . "$evidence_a" . ">Evidence</a><br>");
-                      }
-                      // todo:// find some to auto populate fields like carrier, status similiar to how we do for api/getTowers.php
-                      echo nl2br(@$notes);
-                      break;
-
-                }
-          }
-        }
-?>
+                    <tr class="tr-even">
+                    <td>Street View</td>
+                    <td><?php echo $sv_list ?></td>
+                    </tr>
 </tbody>
 </table>
 </body>
