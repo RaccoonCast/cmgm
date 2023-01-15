a02abc498a7762e869e01483333ba85718627463
Spane Boswell <spaneboswell101@gmail.com>
2023-01-15 13:52:25 -0800

    Misc improvement

--- cmgm/database/includes/edit/latLongMod/copy.php
+++ cmgm/database/includes/edit/latLongMod/copy.php
@@ -1,5 +1,5 @@
 <?php if(!isMobile()) {?>
-<a href="javascript:;" class="nounderline pad-small-link" onclick="copyToClipboard('<?php echo $latitude . "," . $longitude; ?>')">📋</a>
-<a href="javascript:;" class="nounderline pad-small-link" onclick="copyToClipboard('<?php echo $address . ", " . $city . ", " . $state . " " . $zip; ?>')">📋</a>
+<a href="javascript:;" class="nounderline pad-small-link" title="Copy latitude,longitude" onclick="copyToClipboard('<?php echo $latitude . "," . $longitude; ?>')">📋</a>
+<a href="javascript:;" class="nounderline pad-small-link" title="Copy address" onclick="copyToClipboard('<?php echo $address . ", " . $city . ", " . $state . " " . $zip; ?>')">📋</a>
 <!-- <a href="<?php // echo $smart_permit_search; ?>" class="nounderline pad-small-link">📋</a> -->
 <?php } ?>
