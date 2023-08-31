eb8c8c3c4f7c2d88b6575361a7a0e14d59095bd5
Spane Boswell <spaneboswell101@gmail.com>
2023-08-30 20:27:27 -0700

    Remove unused/unworking/etc + button for edit page widgets

--- cmgm/includes/widgets.php
+++ cmgm/includes/widgets.php
@@ -1,12 +1,11 @@
 <?php
 if (!empty($latitude) && !empty($longitude)) $db_map_link = "Map.php?latitude=" . $latitude . "&longitude=" . $longitude . "&zoom=18&carrier=" . @$carrier . "&back=Edit.php?id=" . $id;
 if (@$no_reader != 'true') echo '<a class="widget widget_emote" title="View all info" href="Reader.php?back_url='. $_SERVER["SCRIPT_NAME"] .'&mp-id='.$id.'">🔍</a>';
-if (@$no_delete != 'true') echo '<a class="widget widget_emote" title="Delete" href="Edit.php?id='.$id.'&delete=false&redirPage='.@$redirPage.'">✂️</a>';
+if (@$no_delete != 'true') echo '<a class="widget widget_emote" title="Delete" href="Edit.php?id='.$id.'&delete=false&redirPage='.@$redirPage.'">🗑️</a>';
 if (@$no_map != 'true' && isset($db_map_link)) echo '<a target="_blank" class="widget widget_emote" title="View on Database Map" href="' . $db_map_link . '">🌎</a>';
 if (@$no_edit != 'true') echo '<a class="widget widget_emote" target="_blank" title="Edit" href="Edit.php?id='.$id.'">🔧</a>';
 if (!empty($sv_a) && @$no_street_view != 'true') echo '<a class="widget widget_emote" target="_blank" title="Street View URL" href="https://'.str_replace("https://", "",$sv_a).'">🚗</a>';
 if (basename($_SERVER['SCRIPT_NAME']) == "Edit.php") {
-  echo '<a class="widget widget_emote" title="View all info" href="Edit.php?new">➕</a>';
   if (empty($edit_lock)) {
     echo '<a class="widget widget_emote" title="Lock" href="Edit.php?lock_status=lock&id='.$id.'">🔒</a>';
   } else {
