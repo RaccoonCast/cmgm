a56e4f0c7038d03cb2ddbd6211dbd77a241a8c6b
Spane Boswell <spaneboswell101@gmail.com>
2023-08-30 20:42:20 -0700

    Finally support created_by field in cmgm db

--- cmgm/database/includes/edit/create_new.php
+++ cmgm/database/includes/edit/create_new.php
@@ -1,4 +1,4 @@
 <?php
 $date_added = date("Y-m-d");
-mysqli_query($conn, "INSERT INTO db (id,date_added) VALUES ('$id','$date_added');");
+mysqli_query($conn, "INSERT INTO db (id,date_added,created_by) VALUES ('$id','$date_added','$username');");
 ?>
