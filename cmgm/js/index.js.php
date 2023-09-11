749529f1bbe1f2a8782bacd8cf3e54f90630e87f
Spane Boswell <spaneboswell101@gmail.com>
2023-09-11 13:30:27 -0700

    Remove cm-testmap... full-ier

--- cmgm/js/index.js.php
+++ cmgm/js/index.js.php
@@ -13,9 +13,6 @@ function changeFormAction() {
   document.getElementById("link04").removeAttribute("onclick");
   document.getElementById("link04").setAttribute('type', 'submit');
 
-  document.getElementById("link05").removeAttribute("onclick");
-  document.getElementById("link05").setAttribute('type', 'submit');
-
   document.getElementById("link06").removeAttribute("onclick");
   document.getElementById("link06").setAttribute('type', 'submit');
 
