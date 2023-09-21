87a5f531f53610c8f9641572da8ed5953791832f
Spane Boswell <spaneboswell101@gmail.com>
2023-09-20 18:34:13 -0700

    Remove offset to hide the massive amount of records with 2020-01-01 date_added (fake date)

--- cmgm/fun/includes/count_records_by_day.php
+++ cmgm/fun/includes/count_records_by_day.php
@@ -1,5 +1,5 @@
 <?php
-$sql = "SELECT date_added, COUNT(date_added) AS value_occurrence FROM db WHERE 1=1 $db_vars_unamended GROUP BY date_added ORDER BY value_occurrence DESC LIMIT $limit OFFSET 1";
+$sql = "SELECT date_added, COUNT(date_added) AS value_occurrence FROM db WHERE 1=1 $db_vars_unamended GROUP BY date_added ORDER BY value_occurrence DESC LIMIT $limit";
 $result = $conn->query($sql);
 
 while ($row = $result->fetch_assoc()) {
