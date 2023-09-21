eb9f4231e7d22a1b0087b67b663d32730ee3ed9a
Spane Boswell <spaneboswell101@gmail.com>
2023-09-20 18:34:33 -0700

    Working fix for cmgm.us/fun/ everyone counter.

--- cmgm/fun/includes/count_records_created_by_everyone.php
+++ cmgm/fun/includes/count_records_created_by_everyone.php
@@ -1,7 +1,7 @@
 <?php
-$remove_username_from_db_vars = str_replace('AND created_by = "'.$_GET['username'].'"', '', $db_vars_unamended);
+$string_to_remove = 'AND created_by = \'' . $_GET['username'] . '\'';
+$remove_username_from_db_vars = str_replace($string_to_remove, '', $db_vars_unamended);
 $sql = "SELECT COUNT(*) AS count FROM db WHERE 1=1 $remove_username_from_db_vars";
-
 $result = $conn->query($sql);
 
 while ($row = $result->fetch_assoc()) {
