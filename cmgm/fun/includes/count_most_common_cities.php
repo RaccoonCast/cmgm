b3d0de68db3693101930c63af8bf5ad7a11bdac0
Spane Boswell <spaneboswell101@gmail.com>
2023-09-20 18:33:28 -0700

    Add city link for filtering to cmgm.us/fun/.

--- cmgm/fun/includes/count_most_common_cities.php
+++ cmgm/fun/includes/count_most_common_cities.php
@@ -3,7 +3,13 @@ $sql = "SELECT city,state, COUNT(city) AS city_count FROM db WHERE 1=1 $db_vars_
 $result = $conn->query($sql);
 
 while ($row = $result->fetch_assoc()) {
-  echo $row["city_count"] . " records created in " . $row["city"] . ', ' . $row['state'] . "<br>";
+  $location = $row["city"] . ', ' . $row['state'];
+  $url = removeParameterFromURL($current_url,"city");
+  $url = removeParameterFromURL($url,"state");
+  $location_for_url = $url . "&city=" . $row["city"] . "&state=" . $row['state'];
+
+  $url = '<a href="'. $location_for_url.'">'.$location.'</a>';
+  echo $row["city_count"] . " records created in " . $url . "<br>";
 }
 
 echo "<br>";
