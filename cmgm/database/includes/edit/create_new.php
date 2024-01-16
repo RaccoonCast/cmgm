f6bcd0d2d233d01558188fde098b589a3d4f5105
RaccoonCast <spaneboswell101@gmail.com>
2024-01-15 16:19:48 -0800

    Add time to date_added

--- cmgm/database/includes/edit/create_new.php
+++ cmgm/database/includes/edit/create_new.php
@@ -1,4 +1,4 @@
 <?php
-$date_added = date("Y-m-d");
+$date_added = date("Y-m-d H:i:s");
 mysqli_query($conn, "INSERT INTO db (id,date_added,created_by) VALUES ('$id','$date_added','$username');");
 ?>
