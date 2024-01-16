76e28edc5158eaefa95791cb3fb65ce50817d742
RaccoonCast <spaneboswell101@gmail.com>
2024-01-15 16:23:42 -0800

    Add stats link to footer

--- cmgm/database/includes/footer.php
+++ cmgm/database/includes/footer.php
@@ -24,7 +24,8 @@
    $db_map_link = "Map.php?latitude=" . $latitude . "&longitude=" . $longitude . "&zoom=18&carrier=" . @$carrier;
    $db_list_link = "DB.php?latitude=$latitude&longitude=$longitude&limit=500";
    $gmlink = "https://www.google.com/maps/@?api=1&map_action=map&center=$latitude,$longitude";
-   $uplink = "Upload.php?latitude=$latitude&longitude=$longitude";
+   $uplink = "https://upload.cmgm.us/upload/";
+   $funlink = isset($username) ? "https://cmgm.us/fun/Home.php?limit=15&username=$username" : "https://cmgm.us/fun/Home.php?limit=15";
    ?>
    <div id="footerContainer">
    <footer>
@@ -32,6 +33,7 @@
      <a class="footerlink" target="_blank" href="<?php echo $db_map_link?>">Map</a>
      <a class="footerlink" target="_blank" href="<?php echo $cmlink?>">CellMapper</a>
      <a class="footerlink" target="_blank" href="<?php echo $uplink?>">Upload</a>
+     <a class="footerlink" target="_blank" href="<?php echo $funlink?>">Stats</a>
      <!-- <a class="footerlink" target="_blank" href="<?php // echo $gm2link?>">Permits Map</a> -->
   </footer>
   </div>
