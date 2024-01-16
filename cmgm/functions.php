20acdb2467605105bc69bc565541b17c856d1469
RaccoonCast <spaneboswell101@gmail.com>
2024-01-15 16:25:14 -0800

    grah

--- cmgm/functions.php
+++ cmgm/functions.php
@@ -2,8 +2,9 @@
 date_default_timezone_set('America/Los_Angeles');
 include 'includes/functions/getGetVars.php';
 include 'includes/functions/basic-functions.php';
+include 'includes/functions/checkIfOnUpload.php';
 include 'includes/useridsys/native.php';
-include SITE_ROOT . "/includes/functions/sqlpw.php";
+include 'includes/functions/sqlpw.php';
 include 'includes/functions/css.php';
 include 'includes/functions/headhtml.php';
 include 'includes/functions/common-debug-info.php';
