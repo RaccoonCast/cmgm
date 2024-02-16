9a6292ca5492a56114256921e84487feff3283e5
RaccoonCast <spaneboswell101@gmail.com>
2024-02-16 10:51:25 -0800

    Limit allowed filetypes on upload/

--- cmgm/database/includes/upload/file-select.php
+++ cmgm/database/includes/upload/file-select.php
@@ -1,22 +1,33 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-if ($fileType == "jpg" OR $fileType == "png" OR $fileType == "webp" OR $fileType == "jpeg") { $type = "image"; } else { $type = "misc"; }
-$filename = $type . "-" . substr(str_shuffle(md5(time())),0,25);
-$filePath = "$target_dir" . "$filename.$fileType";
-
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 25000000) {
-  echo "Sorry, your file is too large.";
-  die();
-}
-
-$temp = explode(".", $_FILES["fileToUpload"]["name"]);
-$newfilename = round(microtime(true)) . '.' . end($temp);
-if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $filePath)) {
-$finishedFilename = $filename . "." . $fileType;
-redir ("?finishedFilename=$finishedFilename",0);
+if ($_FILES["fileToUpload"]["size"] > 25) {
+  $target_dir = "uploads/";
+  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+  $fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+  
+  if ($fileType == "jpg" OR $fileType == "png" OR $fileType == "webp" OR $fileType == "jpeg") { $type = "image"; } else { $type = "misc"; }
+  if ($fileType !== "jpg" OR $fileType !== "png" OR $fileType !== "webp" OR $fileType !== "jpeg" OR $fileType) { $type = "image"; } else { $type = "misc"; }
+  $filename = $type . "-" . substr(str_shuffle(md5(time())),0,25);
+  $filePath = "$target_dir" . "$filename.$fileType";
+  
+  // Check file size
+  if ($_FILES["fileToUpload"]["size"] > 25000000) {
+    echo "Sorry, your file is too large.";
+    die();
+  }
+  
+  $allowedFileTypes = ['png', 'jpg', 'pdf', 'webp', 'jpeg'];
+  
+  if (!in_array($fileType, $allowedFileTypes)) {
+      echo "Invalid file type. Allowed types are: " . implode(', ', $allowedFileTypes);
+      die();
+  }
+  
+  
+  $temp = explode(".", $_FILES["fileToUpload"]["name"]);
+  $newfilename = round(microtime(true)) . '.' . end($temp);
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $filePath)) {
+  $finishedFilename = $filename . "." . $fileType;
+  redir ("?finishedFilename=$finishedFilename",0);
+  }
 }
 ?>
