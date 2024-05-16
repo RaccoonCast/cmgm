8b688daaf173052b9e190837ce94f5bace06b867
RaccoonCast <spaneboswell101@gmail.com>
2024-05-16 11:36:41 -0700

    fancccy

--- cmgm/includes/link-conversion-and-handling/convert/dmstodec.php
+++ cmgm/includes/link-conversion-and-handling/convert/dmstodec.php
@@ -9,9 +9,7 @@ preg_match_all('!\d+\.*\d*!', $data, $out);
 $latitude = DMStoDec(@$out[0][0],@$out[0][1],@$out[0][2]);
 $longitude = DMStoDec(@$out[0][3],@$out[0][4],@$out[0][5]);
 if ($latitude != "0" && $longitude != "0") $conv_type = "DMS Coordinates";
-if(strpos($data, "S") !== false) $latitude = "-".$latitude;
-if(strpos($data, "W") !== false) $longitude = "-".$longitude;
-if(strpos($data, "s") !== false) $latitude = "-".$latitude;
-if(strpos($data, "w") !== false) $longitude = "-".$longitude;
+if(stripos($data, "S") !== false) $latitude = "-".$latitude;
+if(stripos($data, "W") !== false) $longitude = "-".$longitude;
 
 ?>
