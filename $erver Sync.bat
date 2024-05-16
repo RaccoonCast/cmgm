f0f887e5724db0a916f4f8f33309d1c0811ba475
RaccoonCast <spaneboswell101@gmail.com>
2024-05-16 11:36:13 -0700

    Maybe kinda probably not working update to server sync script to force it to try reconnecting.

--- $erver Sync.bat
+++ $erver Sync.bat
@@ -1,2 +1,8 @@
-@cd %~dp0
-@cmd /k "@cd server-sync & @call server-sync.bat"
+@echo off
+cd %~dp0
+set looplimit=50
+:loop
+set /a loop+=1
+if %loop% gtr %loopLimit% exit
+cmd /k "@cd server-sync & @call server-sync.bat"
+call :loop
