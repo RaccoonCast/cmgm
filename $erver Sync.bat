af73a5d03ce8d896980ccc5be3d0fe43af594cf7
RaccoonCast <spaneboswell101@gmail.com>
2024-11-25 21:35:28 -0800

    Irrelevant changes

--- $erver Sync.bat
+++ $erver Sync.bat
@@ -1,8 +1,8 @@
 @echo off
 cd %~dp0
-set looplimit=50
+set looplimit=125
 :loop
 set /a loop+=1
 if %loop% gtr %loopLimit% exit
 cmd /k "@cd server-sync & @call server-sync.bat"
-call :loop
+goto :loop
