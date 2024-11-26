6c77f4201096138859298ac41c4b1240f9d0e34b
lukec11 <me@lukec.me>
2024-11-25 22:11:50 -0500

    Support generation of multiple forms
    This isn't the most ideal way to do this, can be iterated/improved upon later, but everything works

--- cmgm/poly/index.php
+++ cmgm/poly/index.php
@@ -8,21 +8,70 @@ $titleOverride = true;
 include "../functions.php";
 
 // Initialize variables from the URL parameters
-isset($_GET['carrier']) && $carrier = $_GET['carrier']; 
+isset($_GET['plmn']) && $carrier = $_GET['plmn']; 
 isset($_GET['rat']) && $rat = $_GET['rat']; 
 isset($_GET['eNB']) && $eNB = $_GET['eNB']; 
 isset($_GET['cellList']) && $cellList = $_GET['cellList'];
 
-// If the form is submitted via POST, handle the data
+// Extract eNBs
+$carrierList = [];
+$ratList = [];
+$enbList = [];
+$cellListList = [];
+
+foreach($_GET as $key => $value) {
+    if (str_starts_with($key, 'plmn')) {
+        array_push($carrierList, $value);
+    } else if (str_starts_with($key, 'rat')) {
+        array_push($ratList, $value);
+    } else if (str_starts_with($key, 'eNB')) {
+        array_push($enbList, $value);
+    } else if (str_starts_with($key, 'cellList')) {
+        array_push($cellListList, $value);
+    }
+ }
+
+ // Check that the length of all lists is the same
+ // (If not, the params are invalid, and we should tell the user to re-do)
+ foreach([$ratList, $enbList, $cellListList] as $value ) {
+    if (count($carrierList) != count($value)) {
+        echo 'bad parameters!';
+        return false;
+    }
+ }
+
+ // If all lists are blank, add template carrier
+ if (count($carrierList) === 0) {
+    // echo 'adding template item';
+    array_push($carrierList, '310410');
+    array_push($ratList, 'LTE');
+    array_push($enbList, '');
+    array_push($cellListList, '');
+ }
+
+//  echo implode(' ', $carrierList), "|", implode(' ', $ratList), "|", implode(' ', $enbList), "|", implode(' ', $cellListList);
+
+
+// If URL parmeters are prefilled, handle the data
 if (isset($carrier, $rat, $eNB, $cellList)) {
 
-    // Prepare POST data
-    $postData = [
-        'plmn' => $carrier,
-        'rat' => $rat,
-        'eNB' => $eNB,
-        'cellList' => html_entity_decode($cellList)
-    ];
+    $postData = [];
+
+    // Same code as HTML generation, just without the HTML
+    foreach ($carrierList as $index => $value) {
+        // Get prefix
+        $namedIndex = ($index === 0) ? '' : '_' . $index;
+
+        // Add carriers to array
+        $postData['plmn' . $namedIndex] = $value;
+
+        // Add others to array
+        $postData['rat' . $namedIndex] = $ratList[$index];
+        $postData['eNB' . $namedIndex] = $enbList[$index];
+        $postData['cellList' . $namedIndex] = $cellListList[$index];
+
+    }
+
 
     // Initialize cURL and send POST request
     $ch = curl_init('https://cmgm.us/poly/web.php');
@@ -50,19 +99,30 @@ if (isset($carrier, $rat, $eNB, $cellList)) {
   <div class="header">
     <h1>eNB Polygon Generator</h1> <span 
 	id="formsContainer">
-    <form id="carrierForm">
-        <select id="plmn" name="plmn" required>
-            <option value="310410"<?php if ($carrier == '310410') echo ' selected'; ?>>AT&T</option>
-            <option value="310260"<?php if ($carrier == '310260') echo ' selected'; ?>>T-Mobile</option>
-            <option value="311480"<?php if ($carrier == '311480') echo ' selected'; ?>>Verizon</option>
-        </select>
-        <select id="rat" name="rat" required>
-            <option value="LTE"<?php if ($rat == 'LTE') echo ' selected'; ?>>LTE</option>
-            <option value="NR"<?php if ($rat == 'NR') echo ' selected'; ?>>NR</option>
-        </select>
-        <input type="number" id="eNB" name="eNB" maxlength="10" required placeholder="eNB" value="<?php echo htmlspecialchars($eNB); ?>" />
-        <input type="text" id="cellList" name="cellList" pattern="^[0-9,]+$" required placeholder="1,2,3 (Cells)" value="<?php echo htmlspecialchars($cellList); ?>" />
-    </form>
+    <?php foreach($carrierList as $index => $value) { ?>
+        <form id="carrierForm">
+
+            <!-- // Set name of index -->
+            <?php $namedIndex = ($index === 0) ? '' : '_' . $index; ?>
+            
+            <!-- PLMN -->
+            <select id="<?php echo "plmn", $namedIndex;?>" name="<?php echo "plmn", $namedIndex;?>" required>
+            <option value="310410"<?php if ($value == '310410') echo ' selected'; ?>>AT&T</option>
+            <option value="310260"<?php if ($value == '310260') echo ' selected'; ?>>T-Mobile</option>
+            <option value="311480"<?php if ($value == '311480') echo ' selected'; ?>>Verizon</option>
+            </select>
+            <!-- RAT -->
+            <select id="<?php echo "rat" , $namedIndex;?>" name="<?php echo "rat" , $namedIndex;?>" required>
+                <option value="LTE"<?php if ($ratList[$index] == 'LTE') echo ' selected'; ?>>LTE</option>
+                <option value="NR"<?php if ($ratList[$index] == 'NR') echo ' selected'; ?>>NR</option>
+            </select>    
+            <!-- eNB -->
+            <input type="number" id="<?php echo "eNB" , $namedIndex;?>" name="<?php echo "eNB" , $namedIndex;?>" maxlength="10" required placeholder="eNB" value="<?php echo $enbList[$index]; ?>" />
+            <!-- Cells -->
+            <input type="text" id="<?php echo "cellList" , $namedIndex;?>" name="<?php echo "cellList" , $namedIndex;?>" pattern="^[0-9,]+$" required placeholder="1,2,3 (Cells)" value="<?php echo $cellListList[$index]; ?>" />
+        </form>
+        <?php } ?>
+    
 </span>
 
 <div id="buttonContainer">
