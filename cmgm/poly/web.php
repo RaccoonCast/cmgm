12f5f8f432b9ee473eaea8d82bbfa91435343f47
RaccoonCast <spaneboswell101@gmail.com>
2024-12-02 13:17:46 -0800

    Minor fix

--- cmgm/poly/web.php
+++ cmgm/poly/web.php
@@ -12,9 +12,9 @@ function error($error) {
 $prettyPost = json_encode($_POST);
 
 // Verify at least the eNB and the carrier name are available in $_POST request.
-if (!isset($_POST['eNB']) || !isset($_POST['plmn'])) {
-    $missingField = !isset($_POST['eNB']) ? "Missing eNB" : "Missing Carrier";
-    error($missingField);
+if (!array_filter(array_keys($_POST), fn($k) => str_starts_with($k, 'eNB')) || 
+    !array_filter(array_keys($_POST), fn($k) => str_starts_with($k, 'plmn'))) {
+    error(!array_filter(array_keys($_POST), fn($k) => str_starts_with($k, 'eNB')) ? "Missing eNB" : "Missing PLMN");
 }
 
 // Call the master
