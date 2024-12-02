70e9d2e0d224daa31b3b4934e5fc6cdf5958d624
RaccoonCast <spaneboswell101@gmail.com>
2024-12-02 13:16:35 -0800

    Size the close button for mobile

--- cmgm/poly/index.php
+++ cmgm/poly/index.php
@@ -120,7 +120,7 @@ $iframe_url = isset($data->URL) ? 'src="' . $data->URL . '&hideui=true"' : 'src=
             <input type="number" class="eNB" name="<?php echo "eNB" . $namedIndex;?>" maxlength="10" required placeholder="eNB" value="<?php echo $enbList[$index]; ?>" /><!-- Cells -->
             <input type="text" class="cellList" name="<?php echo "cellList" , $namedIndex;?>" pattern="^[0-9,]+$" required placeholder="1,2,3 (Cells)" value="<?php echo $cellListList[$index]; ?>" />
             <!-- Delete button -->
-            <input type="button" value="❌" onclick="this.parentElement.remove();"/>
+            <input type="button" value="❌" class="closeButton" onclick="this.parentElement.remove();"/>
          </form>
          <?php } ?>
       </div>
