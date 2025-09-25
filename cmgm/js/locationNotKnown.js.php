e7406bacbcfa07b41ba6ea1f3f7b6ca9e07b8874
lukec11 <me@lukec.me>
2025-09-24 17:03:09 -0700

    Add 2s max time for geolocation
    Failure will pass TIMEOUT error and redirect to default lat/lng

--- cmgm/js/locationNotKnown.js.php
+++ cmgm/js/locationNotKnown.js.php
@@ -2,5 +2,5 @@
 function getPos(position) {location.href = "/?q=" + position.coords.latitude + "," + position.coords.longitude;}
 function positionGetErr() {location.href = "/?q=" + <?php echo $default_latitude; ?> + "," + <?php echo $default_longitude; ?>;}
 
-navigator.geolocation.getCurrentPosition(getPos, positionGetErr);
+navigator.geolocation.getCurrentPosition(getPos, positionGetErr, { timeout: 2000 });
 </script>
