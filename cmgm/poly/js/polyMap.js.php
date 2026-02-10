RaccoonCast/cmgm

commit 8b50b5889d6ee2dc3351948601b59c14a6e6b867

Swap out showIncompletePolygons flag for a default state of showing and add hideIncompletePolygons flag

cmgm/poly/js/polyMap.js.php
@@ -125,8 +125,8 @@ async function fetchAndRenderPolygons() {
 for (const [enb, coords, tac] of Object.entries(enbGroups)) {
 
   // Filter polygons with fewer than three points
-  const showIncompletePolygons = <?php echo(isset($_GET['showIncompletePolygons']) || isset($_GET['filterCells'])) ? 'true' : 'false'; ?>;
-  if (coords.length < 3 && !(showIncompletePolygons || useEnbAverage)) continue;
+  const hideIncompletePolygons = <?php echo(isset($_GET['hideIncompletePolygons']) || isset($_GET['filterCells'])) ? 'true' : 'false'; ?>;
+  if (hideIncompletePolygons && coords.length < 3 && !(useEnbAverage)) continue;
 
   // Get points with extra info
   const pointsWithIndices = coords.map((c, i) => ({

commit 0168c240a8cad06ea32c3b924d272dd3c21154cd

Resolve unresolved bug caused by https://github.com/RaccoonCast/cmgm/commit/8b50b5889d6ee2dc3351948601b59c14a6e6b867

cmgm/poly/js/polyMap.js.php
@@ -125,7 +125,7 @@ async function fetchAndRenderPolygons() {
 for (const [enb, coords, tac] of Object.entries(enbGroups)) {
 
   // Filter polygons with fewer than three points
-  const hideIncompletePolygons = <?php echo(isset($_GET['hideIncompletePolygons']) || isset($_GET['filterCells'])) ? 'true' : 'false'; ?>;
+  const hideIncompletePolygons = <?php echo(isset($_GET['hideIncompletePolygons'])) ? 'true' : 'false'; ?>;
   if (hideIncompletePolygons && coords.length < 3 && !(useEnbAverage)) continue;
 
   // Get points with extra info