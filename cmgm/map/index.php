386f2c2ebcd601a5b9e544a8bfab9624b8ecbd1c
RaccoonCast <spaneboswell101@gmail.com>
2026-03-21 13:26:48 -0700

    '''Fix''' legacy ''new'' map

--- cmgm/map/index.php
+++ cmgm/map/index.php
@@ -5,7 +5,7 @@
 <html lang="en">
 <head>
   <?php
-  include "../includes/headhtml.php";
+  include "../includes/functions/headhtml.php";
   ?>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
