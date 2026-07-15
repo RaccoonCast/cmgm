6c4c11450df36c6de352453e1d8ccde8c2e440f6
RaccoonCast <spaneboswell101@gmail.com>
2026-07-14 22:41:24 -0700

    Modify CellMapper URL converter for Homepage to support the CM' Pin Me links on edit.

--- cmgm/includes/link-conversion-and-handling/convert/cellmapper.net.php
+++ cmgm/includes/link-conversion-and-handling/convert/cellmapper.net.php
@@ -1,6 +1,6 @@
 <?php
 // hey future cast wanting to rewrite this code because you suck less use: https://www.php.net/manual/en/function.parse-url.php
-if (!str_contains($data, 'testmap')) {
+if (str_contains($data, 'map?')) {
   $latitude = strstr($data, "latitude="); // Remove text before latitude= for $latitude.
   $latitude = preg_replace('/[^0-9.-]/', '', strstr($latitude, "&", true));  // Remove text after "&" and remove non-numeric characters for $latitude.
 
@@ -18,23 +18,11 @@ if (!str_contains($data, 'testmap')) {
   if (strpos($data, 'type=LTE') !== false) {$cm_netType = "LTE";}
   if (strpos($data, 'type=NR') !== false) {$cm_netType = "NR";}
 } else {
-  $latitude = strstr($data, "lat="); // Remove text before latitude= for $latitude.
-  $latitude = preg_replace('/[^0-9.-]/', '', strstr($latitude, "&", true));  // Remove text after "&" and remove non-numeric characters for $latitude.
+  // This is designed to catch stuff from overrideData, might catch other stuff by mistake and that's fine I guess.
+  preg_match('/Latitude=([^&]+).*?Longitude=([^&]+)/', $data, $matches);
 
-  $longitude = strstr($data, "lng="); // Remove text before longitude= for $longitude.
-  $longitude = preg_replace('/[^0-9.-]/', '', strstr($longitude, "&", true));  // Remove text after "&" and remove non-numeric characters for $longitude.
-
-  $cm_zoom = strstr($data, "z=") . "&blahblahblah=blah"; // Remove text before longitude= for $longitude. // Add blahblahblah text incase zoom is last paramater in the url
-  $cm_zoom = preg_replace('/[^0-9.-]/', '', strstr($cm_zoom, "&", true));  // Remove text after "&" and remove non-numeric characters for $longitude.
-
-  if (strpos($data, '310/260') !== false) {$carrier = "T-Mobile";}
-  if (strpos($data, '310/120') !== false) {$carrier = "Sprint";}
-  if (strpos($data, '310/410') !== false) {$carrier = "ATT";}
-  if (strpos($data, '311/480') !== false) {$carrier = "Verizon";}
-  if (strpos($data, '313/340') !== false) {$carrier = "Dish";}
-
-  if (strpos($data, 'LTE') !== false) {$cm_netType = "LTE";} // while it may appear that LTE & NR code can probably be outside of this if string they shouldn't because...
-  if (strpos($data, 'NR') !== false) {$cm_netType = "NR";} // non-testmap strings contain &showLTECAonly regardless of whether it is LTE OR NR.
+  $latitude = $matches[1] ?? null;
+  $longitude = $matches[2] ?? null;
 }
 
 $conv_type = "CellMapper";
